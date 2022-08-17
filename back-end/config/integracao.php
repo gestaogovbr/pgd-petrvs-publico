dfc7c3b0062f7c87a8a60965c47b15a2b94106b0
Rycardo-Faryas <ricardo.farias@prf.gov.br>
2022-08-17 10:23:45 -0300

    Após as correções na rotina INTEGRAÇÃO

--- back-end/config/integracao.php
+++ back-end/config/integracao.php
@@ -5,6 +5,10 @@ return [
     'baseUrlunidades' => env('INTEGRACAO_BASE_URL_UNIDADES', ""),
     'baseUrlpessoas' => env('INTEGRACAO_BASE_URL_PESSOAS', ""),
     'validaCertificado' => env('INTEGRACAO_VALIDA_CERTIFICADO', true),
+    'useLocalFiles' => env('INTEGRACAO_USE_LOCAL_FILES', false),
+    'storeLocalFiles' => env('INTEGRACAO_STORE_LOCAL_FILES', false),
+    'localUnidades' => env('INTEGRACAO_LOCAL_UNIDADES', ""),
+    'localServidores' => env('INTEGRACAO_LOCAL_SERVIDORES', ""),
     'token' => env('INTEGRACAO_TOKEN', ""),
     'auto_incluir' => env('INTEGRACAO_AUTO_INCLUIR', false),
     'generate' => [
