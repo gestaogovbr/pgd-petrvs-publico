a530b3d0be2d7a73511d515cd8f26395d6914aa5
Rycardo-Faryas <ricardo.farias@prf.gov.br>
2022-09-12 14:55:00 -0300

    Novos Controllers do Módulo Projetos

---
+++ back-end/app/Http/Controllers/ProjetoRecursoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\ControllerBase;
+use App\Exceptions\ServerException;
+
+class ProjetoRecursoController extends ControllerBase
+{
+    public function checkPermissions($action, $request, $service, $unidade, $usuario) {
+        switch ($action) {
+            case 'STORE':
+                if (!$usuario->hasPermissionTo('MOD_PROJ_REC_INCL')) throw new ServerException("CapacidadeStore", "Inserção não executada");
+                break;
+            case 'UPDATE':
+                if (!$usuario->hasPermissionTo('MOD_PROJ_REC_EDT')) throw new ServerException("CapacidadeStore", "Edição não executada");
+                break;
+            case 'DESTROY':
+                if (!$usuario->hasPermissionTo('MOD_PROJ_REC_EXCL')) throw new ServerException("CapacidadeStore", "Exclusão não executada");
+                break;
+        }
+    }
+}
---
+++ back-end/app/Http/Controllers/ProjetoRegraController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\ControllerBase;
+use App\Exceptions\ServerException;
+
+class ProjetoRegraController extends ControllerBase
+{
+    public function checkPermissions($action, $request, $service, $unidade, $usuario) {
+        switch ($action) {
+            case 'STORE':
+                if (!$usuario->hasPermissionTo('MOD_PROJ_REG_INCL')) throw new ServerException("CapacidadeStore", "Inserção não executada");
+                break;
+            case 'UPDATE':
+                if (!$usuario->hasPermissionTo('MOD_PROJ_REG_EDT')) throw new ServerException("CapacidadeStore", "Edição não executada");
+                break;
+            case 'DESTROY':
+                if (!$usuario->hasPermissionTo('MOD_PROJ_REG_EXCL')) throw new ServerException("CapacidadeStore", "Exclusão não executada");
+                break;
+        }
+    }
+}
