fdbfa6a33eb6f18635a86c26a016599824763e27
CarlosBCIII <88202826+CarlosBCIII@users.noreply.github.com>
2023-05-11 14:51:22 -0300

    Seeder Cursos
    Seeder Cursos

--- back-end/database/seeders/CursoSeeder.php
+++ back-end/database/seeders/CursoSeeder.php
@@ -51,6 +51,10 @@ class CursoSeeder extends Seeder
     ['area'=>'ae78a2a4-65a6-431d-ad98-60d37a491e43','nome'=>'de transportes','titulo'=>'GRAD_BAC'],
     ['area'=>'ae78a2a4-65a6-431d-ad98-60d37a491e43','nome'=>'Naval','titulo'=>'GRAD_BAC'],
     ['area'=>'ae78a2a4-65a6-431d-ad98-60d37a491e43','nome'=>'Aeroespacial','titulo'=>'GRAD_BAC'],
+    ['area'=>'ae78a2a4-65a6-431d-ad98-60d37a491e43','nome'=>'Perícias de Engenharia','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'ae78a2a4-65a6-431d-ad98-60d37a491e43','nome'=>'Segurança do Trabalho','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'ae78a2a4-65a6-431d-ad98-60d37a491e43','nome'=>'MBA em Gestão para Engenharias','titulo'=>'ESPECIAL'],//MBA
+   
      /* Ciências da Saúde */
     ['area'=>'aad04dcb-4dbe-419d-87a3-eb4e661eae70','nome'=>'Medicina','titulo'=>'GRAD_BAC'],
     ['area'=>'aad04dcb-4dbe-419d-87a3-eb4e661eae70','nome'=>'Enfermagem','titulo'=>'GRAD_BAC'],
@@ -62,6 +66,10 @@ class CursoSeeder extends Seeder
     ['area'=>'aad04dcb-4dbe-419d-87a3-eb4e661eae70','nome'=>'Terapia Ocupacional','titulo'=>'GRAD_BAC'],
     ['area'=>'aad04dcb-4dbe-419d-87a3-eb4e661eae70','nome'=>'Educação Física','titulo'=>'GRAD_BAC'],
     ['area'=>'aad04dcb-4dbe-419d-87a3-eb4e661eae70','nome'=>'Biomedicina','titulo'=>'GRAD_BAC'],
+    ['area'=>'aad04dcb-4dbe-419d-87a3-eb4e661eae70','nome'=>'Bio Medicina Estética','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'aad04dcb-4dbe-419d-87a3-eb4e661eae70','nome'=>'Nutrição Clinica e Hospitalar','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'aad04dcb-4dbe-419d-87a3-eb4e661eae70','nome'=>'MBA em Gestão de Saúde','titulo'=>'ESPECIAL'],//MBA
+    ['area'=>'aad04dcb-4dbe-419d-87a3-eb4e661eae70','nome'=>'MBA em Serviços de Saúde','titulo'=>'ESPECIAL'],//MBA
      /* Ciências Humanas */
     ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Filosofia','titulo'=>'GRAD_BAC'],
     ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Sociologia','titulo'=>'GRAD_BAC'],
@@ -73,7 +81,14 @@ class CursoSeeder extends Seeder
     ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Educação','titulo'=>'GRAD_BAC'],
     ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Ciência Política','titulo'=>'GRAD_BAC'],
     ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Teologia','titulo'=>'GRAD_BAC'],
-    
+    ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Gestão de Pessoas','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'MBA em Gestão Empresarial','titulo'=>'ESPECIAL'],//MBA
+    ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'MBA em Gestão de Projetos','titulo'=>'ESPECIAL'],//MBA
+    ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'MBA Gestão Estratégica de Pessoas','titulo'=>'ESPECIAL'],//MBA
+    ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Gestão Operacional','titulo'=>'ESPECIAL'],//Especialização PRF
+    ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Inteligência','titulo'=>'ESPECIAL'],//Especialização PRF
+    ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Corregedoria','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Gestão Executiva','titulo'=>'ESPECIAL'],//Especialização PRF
      /* Ciências Agrárias */
     ['area'=>'4f4b185e-c257-44ab-a84d-5d3aed870e29','nome'=>'Agronomia','titulo'=>'GRAD_BAC'],
     ['area'=>'4f4b185e-c257-44ab-a84d-5d3aed870e29','nome'=>'Recursos Florestais (Incluindo Engenharia Florestal)','titulo'=>'GRAD_BAC'],
@@ -82,6 +97,9 @@ class CursoSeeder extends Seeder
     ['area'=>'4f4b185e-c257-44ab-a84d-5d3aed870e29','nome'=>'Medicina Veterinária','titulo'=>'GRAD_BAC'],
     ['area'=>'4f4b185e-c257-44ab-a84d-5d3aed870e29','nome'=>'Recursos Pesqueiros','titulo'=>'GRAD_BAC'],
     ['area'=>'4f4b185e-c257-44ab-a84d-5d3aed870e29','nome'=>'Engenharia De Pesca E De Alimentos','titulo'=>'GRAD_BAC'],
+    ['area'=>'4f4b185e-c257-44ab-a84d-5d3aed870e29','nome'=>'Citicultura','titulo'=>'ESPECIAL'],//Especialização PRF
+    ['area'=>'4f4b185e-c257-44ab-a84d-5d3aed870e29','nome'=>'Fitotecnia','titulo'=>'ESPECIAL'],//Especialização PRF
+    ['area'=>'4f4b185e-c257-44ab-a84d-5d3aed870e29','nome'=>'MBA em Agronegócio','titulo'=>'ESPECIAL'],//MBA
    
      /* Ciências Sociais Aplicadas */
     ['area'=>'085122b6-56a6-4eea-953a-0a4c215a72af','nome'=>'Direito','titulo'=>'GRAD_BAC'],
@@ -96,6 +114,13 @@ class CursoSeeder extends Seeder
     ['area'=>'085122b6-56a6-4eea-953a-0a4c215a72af','nome'=>'Turismo','titulo'=>'GRAD_BAC'],
     ['area'=>'085122b6-56a6-4eea-953a-0a4c215a72af','nome'=>'Hotelaria','titulo'=>'GRAD_BAC'],
     ['area'=>'085122b6-56a6-4eea-953a-0a4c215a72af','nome'=>'Relações Internacionais','titulo'=>'GRAD_BAC'],
+    ['area'=>'085122b6-56a6-4eea-953a-0a4c215a72af','nome'=>'Direito Digital','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'085122b6-56a6-4eea-953a-0a4c215a72af','nome'=>'Finanças','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'085122b6-56a6-4eea-953a-0a4c215a72af','nome'=>'Administração Pública','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Gestão Pública','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'81531e04-8a54-488c-9dd9-109242c96709','nome'=>'Administração e Logística','titulo'=>'ESPECIAL'],//Especialização PRF
+    ['area'=>'085122b6-56a6-4eea-953a-0a4c215a72af','nome'=>'MBA em Administração Estratégica','titulo'=>'ESPECIAL'],//MBA
+   
      /* Linguistica Letras e Artes */
     ['area'=>'3ca47239-2376-4492-b9b3-ede6e3521a2e','nome'=>'Linguística','titulo'=>'GRAD_BAC'],
     ['area'=>'3ca47239-2376-4492-b9b3-ede6e3521a2e','nome'=>'Letras','titulo'=>'GRAD_BAC'],
@@ -104,7 +129,10 @@ class CursoSeeder extends Seeder
     ['area'=>'3ca47239-2376-4492-b9b3-ede6e3521a2e','nome'=>'Dança','titulo'=>'GRAD_BAC'],
     ['area'=>'3ca47239-2376-4492-b9b3-ede6e3521a2e','nome'=>'Teatro','titulo'=>'GRAD_BAC'],
     ['area'=>'3ca47239-2376-4492-b9b3-ede6e3521a2e','nome'=>'Cinema','titulo'=>'GRAD_BAC'],
-    ['area'=>'3ca47239-2376-4492-b9b3-ede6e3521a2e','nome'=>'Fotografia','titulo'=>'GRAD_BAC']
+    ['area'=>'3ca47239-2376-4492-b9b3-ede6e3521a2e','nome'=>'Fotografia','titulo'=>'GRAD_BAC'],
+    ['area'=>'3ca47239-2376-4492-b9b3-ede6e3521a2e','nome'=>'Linguagem','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'3ca47239-2376-4492-b9b3-ede6e3521a2e','nome'=>'Liguistica','titulo'=>'ESPECIAL'],//Especialização
+    ['area'=>'3ca47239-2376-4492-b9b3-ede6e3521a2e','nome'=>'MBA em Lingua Portuguesa e Linguistica','titulo'=>'ESPECIAL'],//MBA
     ];
       
         foreach($array_cursos as $curso){
