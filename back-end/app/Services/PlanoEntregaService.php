d239888b47cd64ff1d0833e99c0927969001785a
pablosilva.economia <pablo.silva@economia.gov.br>
2023-06-01 14:32:11 -0300

    Commit validação das regras do plano de entregas

--- back-end/app/Services/PlanoEntregaService.php
+++ back-end/app/Services/PlanoEntregaService.php
@@ -6,8 +6,10 @@ use App\Models\Unidade;
 use App\Models\PlanoEntrega;
 use App\Traits\UseDataFim;
 use App\Exceptions\ServerException;
+use App\Models\Programa;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use DateTime;
 use Throwable;
 
 class PlanoEntregaService extends ServiceBase
@@ -228,7 +230,7 @@ class PlanoEntregaService extends ServiceBase
      * Um Plano de Entregas é válido se não foi deletado, nem cancelado, nem arquivado.
      * @param PlanoEntrega $planoEntrega  
      */
-    public function isPlanoEntregaValido($planoEntrega): bool {
+    public function isPlanoEntregaValido($planoEntrega): bool { 
         return !$planoEntrega->data_fim && !$planoEntrega->data_cancelamento && !$planoEntrega->data_arquivamento;
     }
 
@@ -323,8 +325,48 @@ class PlanoEntregaService extends ServiceBase
         return true;
     }
 
-    public function validateStore(){
-
+    /**
+    * Verifica se as  datas do plano de entrega se encaixam na duração do Programa de gestão
+    */
+    public function validateStore($planoEntrega)
+    {
+        return $this->verificaDuracaoPlano($planoEntrega) && $this->verificaDatasEntregas($planoEntrega);
     }
 
+    /**
+    * Verifica se as  datas do plano de entrega se encaixam na duração do Programa de gestão
+    */
+    public function verificaDuracaoPlano($planoEntrega)
+    {
+        $result = true;
+        $programa = Programa::find($planoEntrega["programa_id"]);
+        if ($programa->prazo_execucao > 0) {
+            $dataInicio = new DateTime($planoEntrega["inicio"]);
+            $dataFim = new DateTime($planoEntrega["fim"]);
+            $diff = $dataInicio->diff($dataFim);
+            if ($diff->days > $programa->prazo_execucao) {
+                $result = false;
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * Verifica se as datas de início e fim das entregas do plano de entrega se encaixam na duração do Programa de gestão (true para caso esteja tudo ok)
+     */
+    public function verificaDatasEntregas($planoEntrega)
+    {
+        $result = true;
+        $dataInicio = new DateTime($planoEntrega["inicio"]);
+        $dataFim = new DateTime($planoEntrega["fim"]);
+        if ($planoEntrega["entregas"]) {
+            foreach ($planoEntrega["entregas"] as $entrega) {
+                $entregaInicio = new DateTime($entrega->inicio);
+                $entregaFim = new DateTime($entrega->fim);
+                $result = $result && ($dataInicio <= $entregaInicio) && ($dataFim >= $entregaFim);
+            }
+        }
+        return $result;
+    }
+    
 }
--- resources/documentacao/Plano de Entrega/plano_de_entregas.md
+++ resources/documentacao/Plano de Entrega/plano_de_entregas.md
@@ -220,9 +220,12 @@
     1. (RN_ENT_PENT_1_1) As entregas que compõem um Plano de Entregas pertencem todas à Unidade Executora do Plano;
     2. (RN_ENT_PENT_1_2) As entregas incluídas/alteradas na criação/edição de um Plano de Entregas precisam ser homologadas pelo gestor da unidade hierarquicamente superior;
     3. (RN_ENT_PENT_1_3) As entregas incluídas/alteradas na criação/edição de um Plano de Entregas precisam estar dentro do prazo de execução de um plano de entregas;
-    3. (RN_ENT_PENT_1_3) As entregas incluídas/alteradas na criação/edição de um Plano de Entregas precisam estar dentro do prazo de execução de um plano de entregas;
     
+## REGRAS DE INTERFACE APLICADAS ÀS VIEWS
 
+1. (RI_ENT_PENT_1) O botão Aderir, na toolbar, deverá ser exibido sempre, mas para ficar habilitado:
+    - as abas de objetivos e processos na tela de inserção das entregas apenas serão exibidas se o usuário selecionar o planejamento e/ou a cadeia de valor na tela anterior ;
+    
 ## REGRAS DE NEGÓCIO A SEREM DISCUTIDAS
 
 - O gestor da unidade de execução **deverá também cadastrar, quando da elaboração do Plano de Entregas,** os tipos de "ocorrência" (ocorrências tradicionais, férias, cursos, etc) que não possuem meta, prazo ou cliente. Essas ocorrências poderão ser selecionadas pelos participantes quando da elaboração do plano de trabalho, com respectiva alocação, ao selecionar a opção: Não vinculada a entrega.
