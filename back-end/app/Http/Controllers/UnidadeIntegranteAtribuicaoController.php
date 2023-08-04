c71080783f32c8421c19782f1fda52e1067d0a05
Ricardo Farias <ricardo.farias@prf.gov.br>
2023-08-04 00:47:42 -0300

    COMMIT PARCIAL - correções no salvamento de gestor e gestor substituto de unidades; - inclusão da lotação no form de usuário;

---
+++ back-end/app/Http/Controllers/UnidadeIntegranteAtribuicaoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\ControllerBase;
+
+class UnidadeIntegranteAtribuicaoController extends ControllerBase {
+    
+    public function checkPermissions($action, $request, $service, $unidade, $usuario) {}
+
+}
