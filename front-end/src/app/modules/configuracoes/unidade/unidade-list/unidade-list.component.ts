c2f35ff7c8384013e1059893b9d86217ace0b2af
Geisimar Rech <geisimar.rech87@gmail.com>
2023-09-16 12:34:39 +0000

    Feature/arvore unidades

--- front-end/src/app/modules/configuracoes/unidade/unidade-list/unidade-list.component.ts
+++ front-end/src/app/modules/configuracoes/unidade/unidade-list/unidade-list.component.ts
@@ -1,14 +1,7 @@
 import { Component, Injector, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { GridComponent } from 'src/app/components/grid/grid.component';
-<<<<<<< 955a2fd673caea18c7e348fba524a8e48a29dbb6
 import { TabsComponent } from 'src/app/components/tabs/tabs.component';
-=======
-import { InputSwitchComponent } from 'src/app/components/input/input-switch/input-switch.component';
-import { ToolbarButton } from 'src/app/components/toolbar/toolbar.component';
-import { CidadeDaoService } from 'src/app/dao/cidade-dao.service';
-import { EntidadeDaoService } from 'src/app/dao/entidade-dao.service';
->>>>>>> 65d1ee1109dfab421991abf0d8ce81bf742a23dc
 import { UnidadeDaoService } from 'src/app/dao/unidade-dao.service';
 import { Unidade } from 'src/app/models/unidade.model';
 import { PageListBase } from 'src/app/modules/base/page-list-base';
@@ -21,16 +14,7 @@ import { LookupItem } from 'src/app/services/lookup.service';
 })
 export class UnidadeListComponent extends PageListBase<Unidade, UnidadeDaoService> {
   @ViewChild(GridComponent, { static: false }) public grid?: GridComponent;
-<<<<<<< 955a2fd673caea18c7e348fba524a8e48a29dbb6
   @ViewChild(TabsComponent, { static: false }) public tabs?: TabsComponent;
-=======
-  @ViewChild('instituidora', { static: false }) public instituidora?: InputSwitchComponent;
-
-  public cidadeDao: CidadeDaoService;
-  public entidadeDao: EntidadeDaoService;
-  public buttons: ToolbarButton[] = [];
-  public unidadesJaVinculadas: string[] = [];
->>>>>>> 65d1ee1109dfab421991abf0d8ce81bf742a23dc
 
   constructor(public injector: Injector) {
     super(injector, Unidade, UnidadeDaoService);
