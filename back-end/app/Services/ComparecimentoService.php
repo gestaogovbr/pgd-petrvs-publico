128433991bfdce86e2838803ba8b89d1f19bf027
edsondario <edson.dario@gmail.com>
2023-09-20 16:00:27 -0300

    IN24_2023 - Comparecimento. Início."

--- back-end/app/Services/ComparecimentoService.php
+++ back-end/app/Services/ComparecimentoService.php
@@ -2,10 +2,9 @@
 
 namespace App\Services;
 
-use App\Models\PlanoTrabalhoConsolidacao;
 use App\Models\Comparecimento;
-use App\Models\Usuario;
 use App\Models\Unidade;
+use App\Models\PlanoTrabalhoConsolidacao;
 
 use App\Services\ServiceBase;
 use Illuminate\Support\Facades\Artisan;
@@ -20,34 +19,22 @@ class ComparecimentoService extends ServiceBase {
      * @return Object
    */
 
-    /*
-    public function store($dataOrEntity, $unidade, $transaction = true) {
-        try{
-            parent::store($dataOrEntity, $unidade, false);
-        } catch (\Exception $e) {
-            throw $e;
-        }
-    }
-    */
-
-    public function extraStore($dataOrEntity, $usuario,$unidade, 
-        $plano_trabalho_consolidacao, $data_comparecimento, $action) {
+    public function proxyStore($unidade, 
+        $plano_trabalho_consolidacao, $data_comparecimento) {
         
         try {
             $unidade_id = Unidade::find($unidade->id);
-            $usuario_id = Usuario::find($usuario->id);
             $plano_trabalho_consolidacao_id = PlanoTrabalhoConsolidacao::find($plano_trabalho_consolidacao->id);
 
             $comparecimento = new Comparecimento([
               'data_comparecimento'=> $data_comparecimento,
               'unidade_id' => $unidade_id,
-              'usuario_id' => $usuario_id,
               'plano_trabalho_consolidacao_id' => $plano_trabalho_consolidacao_id,
             ]);
             $comparecimento->save();
             return $comparecimento;
         } catch (\Exception $e) {
               throw $e;
-          }  
+          }
     }
 }
