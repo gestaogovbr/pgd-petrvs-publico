083c92e979a5bb38c783f22acaa35a5c4c69410a
Genisson <genisson.albuquerque@prf.gov.br>
2023-09-20 23:28:39 -0300

    - Diversos

--- front-end/src/app/modules/gestao/plano-trabalho/plano-trabalho-list-accordeon/plano-trabalho-list-accordeon.component.ts
+++ front-end/src/app/modules/gestao/plano-trabalho/plano-trabalho-list-accordeon/plano-trabalho-list-accordeon.component.ts
@@ -50,7 +50,7 @@ export class PlanoTrabalhoListAccordeonComponent extends PageFrameBase {
       let agora = (new Date()).getTime();
       this.planos = dados.planos;
       for(var i = 0; i < this.planos.length; i++) {
-        if(this.util.asDate(this.planos[i].data_inicio)!.getTime() <= agora && agora <= this.util.asDate(this.planos[i].data_fim)!.getTime() && ["ATIVO", "CONCLUIDO"].includes(this.planos[i].status)) {
+        if(this.util.asTimestamp(this.planos[i].data_inicio) <= agora && agora <= this.util.asTimestamp(this.planos[i].data_fim) && ["ATIVO", "CONCLUIDO"].includes(this.planos[i].status)) {
           this.selectedIndex = i;
         }
       }
