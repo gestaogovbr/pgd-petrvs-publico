8df5015bf85749516c89a65518a87fb441f6c2dd
edsondario <edson.dario@gmail.com>
2023-10-23 00:32:01 -0300

    Remoção de obrigatoriedade de código da unidade no frontend

--- front-end/src/app/modules/configuracoes/unidade/unidade-form/unidade-form.component.ts
+++ front-end/src/app/modules/configuracoes/unidade/unidade-form/unidade-form.component.ts
@@ -91,7 +91,7 @@ export class UnidadeFormComponent extends PageFormBase<Unidade, UnidadeDaoServic
 
   public validate = (control: AbstractControl, controlName: string) => {
     let result = null;
-    if(['codigo', 'sigla', 'nome', 'cidade_id', 'entidade_id','unidade_pai_id'].indexOf(controlName) >= 0 && !control.value?.length) {
+    if(['sigla', 'nome', 'cidade_id', 'entidade_id','unidade_pai_id'].indexOf(controlName) >= 0 && !control.value?.length) {
       result = "Obrigatório";
     }
     return result;
