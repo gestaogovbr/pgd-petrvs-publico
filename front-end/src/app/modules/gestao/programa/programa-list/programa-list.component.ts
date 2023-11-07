c1b7cb00628aae6d94b29a3b0ea71ae3bee17a87
marcelomaizman <137897604+marcelomaizman@users.noreply.github.com>
2023-11-07 14:36:47 -0300

    Nascimento de usuário e Link PGD

--- front-end/src/app/modules/gestao/programa/programa-list/programa-list.component.ts
+++ front-end/src/app/modules/gestao/programa/programa-list/programa-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Injector, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { GridComponent } from 'src/app/components/grid/grid.component';
+import { ToolbarButton } from 'src/app/components/toolbar/toolbar.component';
 import { ProgramaDaoService } from 'src/app/dao/programa-dao.service';
 import { Programa } from 'src/app/models/programa.model';
 import { PageListBase } from 'src/app/modules/base/page-list-base';
@@ -12,6 +13,8 @@ import { PageListBase } from 'src/app/modules/base/page-list-base';
 })
 export class ProgramaListComponent extends PageListBase<Programa, ProgramaDaoService> {
   @ViewChild(GridComponent, { static: false }) public grid?: GridComponent;
+
+  public buttons: ToolbarButton[] = [];
 
   constructor(public injector: Injector, dao: ProgramaDaoService) {
     super(injector, Programa, ProgramaDaoService);
@@ -42,7 +45,13 @@ export class ProgramaListComponent extends PageListBase<Programa, ProgramaDaoSer
         onClick: (programa: Programa) => this.go.navigate({route: ["gestao", "desdobramento", programa.id, "programa"]})
       });
     }
-   
+    
+    this.buttons.push({
+      icon: "bi bi-box-arrow-up-right",
+      color: "btn-outline-secondary",
+      label: "Norma PGD - IN 24",
+      onClick: () => window.open("https://www.in.gov.br/en/web/dou/-/instrucao-normativa-conjunta-seges-sgprt-/mgi-n-24-de-28-de-julho-de-2023-499593248")
+    });
   }
 
   public filterWhere = (filter: FormGroup) => {
