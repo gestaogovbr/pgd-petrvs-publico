42b65d95a1fbed17f1b65ca0f5530af527a4ffb2
Genisson <genisson.albuquerque@prf.gov.br>
2023-11-14 18:51:47 -0300

    - Modelagem OKR

---
+++ back-end/database/migrations/tenant/2023_11_14_174945_create_okrs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOkrsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('okrs', function (Blueprint $table) {
+            // Configurações:
+            $table->uuid('id');
+            $table->primary('id');
+            $table->timestamps();
+            $table->softDeletes();
+            // Campos:
+            $table->dateTime('data_inicio')->comment("Data de início do OKR");
+            $table->dateTime('data_fim')->comment("Data final do OKR");
+            $table->dateTime('data_arquivamento')->nullable()->comment("Data de arquivamento do OKR");
+            $table->string('nome', 256)->comment("Nome do OKR");
+            // Chaves estrangeiras:
+            $table->foreignUuid('unidade_id')->nullable()->constrained()->onDelete('restrict')->onUpdate('cascade')->comment("Unidade do OKR (opcional)");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('okrs');
+    }
+}
---
+++ back-end/database/migrations/tenant/2023_11_14_283243_create_planos_entregas_entregas_resultados_chaves_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanosEntregasEntregasResultadosChavesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planos_entregas_entregas_resultados_chaves', function (Blueprint $table) {
+            // Configurações:
+            $table->uuid('id');
+            $table->primary('id');
+            $table->timestamps();
+            $table->softDeletes();
+            // Campos:
+            $table->uuid('okr_objetivo_resultado_chave_id')->comment("Resultado chave do OKR");
+            $table->uuid('entrega_id')->comment("Entrega do Plano de Entregas à qual está vinculado");
+            // Chaves estrangeiras:
+            $table->foreign('okr_objetivo_resultado_chave_id', 'fk_plan_entr_entr_okr_id_resultado_chave')
+                ->references('id')
+                ->on('okrs_objetivos_resultados_chaves')
+                ->constrained('okrs_objetivos_resultados_chaves')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+            $table->foreign('entrega_id', 'fk_plan_ent_ent_id_plan_entr_entr_okr_id')
+                ->references('id')
+                ->on('planos_entregas_entregas')
+                ->constrained('planos_entregas_entregas')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planos_entregas_entregas_resultados_chaves');
+    }
+}
