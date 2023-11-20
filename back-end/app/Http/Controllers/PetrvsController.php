583a021f1a44c3d59a3d598d51ac391325e67a78
marco.coelho <marco.coelho@firstbps.com.br>
2023-11-20 19:50:47 -0400

    -  Correção da Tela Login
    Signed-off-by: marco.coelho <marco.coelho@firstbps.com.br>

--- back-end/app/Http/Controllers/PetrvsController.php
+++ back-end/app/Http/Controllers/PetrvsController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tenant;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\View;
 use App\Services\PetrvsService;
 use App\Http\Controllers\ControllerBase;
+use Stancl\Tenancy\Database\Models\Domain;
 use Throwable;
 
 class PetrvsController extends ControllerBase
@@ -14,23 +16,55 @@ class PetrvsController extends ControllerBase
     public function checkPermissions($action, $request, $service, $unidade, $usuario) { }
 
     public function environmentConfig(Request $request) {
-        $app_config = config("app");
-        $petrvs_config = config("petrvs");
-        $google_config = config("google");
+        $url = $request->url();
+        $parsedUrl = parse_url($url);
+        // Obtém o host (domínio) do URL
+        $domain = $parsedUrl['host'];
+
+        $tenant = Domain::where('domain', $domain)->with('tenant')->first();
+
+        if(!$tenant){
+            $app_config = config("app");
+            $petrvs_config = config("petrvs");
+            $google_config = config("google");
+            $config = json_encode([
+                "api_url" => $app_config["url"],
+                "app_env" => $app_config["env"],
+                "entidade" => $petrvs_config["entidade"],
+                "suporte_url" => $petrvs_config["suporte"],
+                "logo_url" => $petrvs_config["logo"],
+                "versao" => $app_config["version"],
+                "login" => [
+                    "google_client_id" => $google_config["client_id"],
+                    "gsuit" => $petrvs_config["login"]["gsuit"],
+                    "azure" => $petrvs_config["login"]["azure"],
+                    "institucional" => $petrvs_config["login"]["institucional"],
+                    "firebase" => $petrvs_config["login"]["firebase"],
+                    "user_password" => $petrvs_config["login"]["user-password"],
+                    "login_unico" => $petrvs_config["login"]["login-unico"]
+                ]
+            ]);
+            return response()->view('environment-config', ["config" => $config])
+                ->header('Content-Type', 'application/javascript');
+        }
+
+        $tenant=json_decode($tenant['tenant'],true);
+
         $config = json_encode([
-            "api_url" => $app_config["url"],
-            "app_env" => $app_config["env"],
-            "entidade" => $petrvs_config["entidade"],
-            "suporte_url" => $petrvs_config["suporte"],
-            "logo_url" => $petrvs_config["logo"],
-            "versao" => $app_config["version"],
+            "api_url" => $tenant["dominio_url"],
+            "app_env" => config("app.env"),
+            "entidade" => $tenant["id"],
+            "suporte_url" =>$tenant["dominio_url"],
+            "logo_url" => null,
+            "versao" => $tenant["version"],
             "login" => [
-                "google_client_id" => $google_config["client_id"],
-                "gsuit" => $petrvs_config["login"]["gsuit"],
-                "azure" => $petrvs_config["login"]["azure"],
-                "institucional" => $petrvs_config["login"]["institucional"],
-                "firebase" => $petrvs_config["login"]["firebase"],
-                "user_password" => $petrvs_config["login"]["user-password"]
+                "google_client_id" =>   $tenant["login_google_client_id"],
+                "gsuit" =>              true,
+                "azure" =>              true,
+                "institucional" =>      true,
+                "firebase" =>           true,
+                "user_password" =>      true,
+                "login_unico" =>        true
             ]
         ]);
         return response()->view('environment-config', ["config" => $config])
