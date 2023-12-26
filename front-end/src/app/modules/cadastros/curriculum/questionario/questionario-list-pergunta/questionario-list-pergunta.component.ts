5697c40d392e1fe524bdea6a378bd32c2b9ce9b8
CarlosBCIII <88202826+CarlosBCIII@users.noreply.github.com>
2023-12-26 10:18:41 -0300

    Add ZN

--- front-end/src/app/modules/cadastros/curriculum/questionario/questionario-list-pergunta/questionario-list-pergunta.component.ts
+++ front-end/src/app/modules/cadastros/curriculum/questionario/questionario-list-pergunta/questionario-list-pergunta.component.ts
@@ -57,7 +57,6 @@ export class QuestionarioListPerguntaComponent extends PageFrameBase {
     this.dao!.query({where: [["questionario_id", "==", this.questionarioId]], orderBy: [["sequencia", "asc"]]}).asPromise().then(rows => {
       this.items = (rows as QuestionarioPergunta[]) || [];
     });
-  
   }  
 
 }
