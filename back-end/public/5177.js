cd43ed5529fe5b5867436976a752cf6308ac2332
marcelomaizman <137897604+marcelomaizman@users.noreply.github.com>
2024-01-11 12:02:34 -0300

    Build

--- back-end/public/5177.js
+++ back-end/public/5177.js
@@ -1386,7 +1386,8 @@ class PlanoEntregaFormEntregaComponent extends src_app_modules_base_page_form_ba
           default:
             break;
         }
-        if (entregaItem.etiquetas) _this9.loadEtiquetas();
+        //if (entregaItem.etiquetas) this.loadEtiquetas();
+        _this9.loadEtiquetas();
         if (entregaItem.checklist) _this9.loadChecklist();
         _this9.calculaRealizado();
       }
@@ -1394,7 +1395,9 @@ class PlanoEntregaFormEntregaComponent extends src_app_modules_base_page_form_ba
   }
   loadEtiquetas() {
     this.etiquetas = this.util.merge(this.entrega?.selectedEntity.etiquetas, this.unidade?.selectedEntity.etiquetas, (a, b) => a.key == b.key);
+    this.etiquetas = this.util.merge(this.etiquetas, this.auth.usuario.config?.etiquetas, (a, b) => a.key == b.key); //
   }
+
   loadChecklist() {
     const modeloEntrega = this.entrega?.selectedEntity;
     let checkAdd = modeloEntrega.checklist.map(a => {
@@ -1544,7 +1547,7 @@ _class.ɵcmp = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_27__["ɵɵdef
       _angular_core__WEBPACK_IMPORTED_MODULE_27__["ɵɵadvance"](1);
       _angular_core__WEBPACK_IMPORTED_MODULE_27__["ɵɵproperty"]("size", 6)("stepValue", 0.01);
       _angular_core__WEBPACK_IMPORTED_MODULE_27__["ɵɵadvance"](1);
-      _angular_core__WEBPACK_IMPORTED_MODULE_27__["ɵɵproperty"]("collapsed", true);
+      _angular_core__WEBPACK_IMPORTED_MODULE_27__["ɵɵproperty"]("collapsed", false);
       _angular_core__WEBPACK_IMPORTED_MODULE_27__["ɵɵadvance"](2);
       _angular_core__WEBPACK_IMPORTED_MODULE_27__["ɵɵproperty"]("size", 6)("control", ctx.form.controls.etiquetas)("addItemHandle", ctx.addItemHandleEtiquetas.bind(ctx));
       _angular_core__WEBPACK_IMPORTED_MODULE_27__["ɵɵadvance"](1);
