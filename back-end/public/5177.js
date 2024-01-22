8a42a56a5a0137e57ed7e35ba09c28f1042161b0
marcelomaizman <137897604+marcelomaizman@users.noreply.github.com>
2024-01-22 13:03:14 -0300

    Build

--- back-end/public/5177.js
+++ back-end/public/5177.js
@@ -1396,7 +1396,8 @@ class PlanoEntregaFormEntregaComponent extends src_app_modules_base_page_form_ba
           default:
             break;
         }
-        if (entregaItem.etiquetas) _this9.loadEtiquetas();
+        //if (entregaItem.etiquetas) this.loadEtiquetas();
+        _this9.loadEtiquetas();
         if (entregaItem.checklist) _this9.loadChecklist();
         _this9.calculaRealizado();
       }
@@ -1404,6 +1405,7 @@ class PlanoEntregaFormEntregaComponent extends src_app_modules_base_page_form_ba
   }
   loadEtiquetas() {
     this.etiquetas = this.util.merge(this.entrega?.selectedEntity.etiquetas, this.unidade?.selectedEntity.etiquetas, (a, b) => a.key == b.key);
+    this.etiquetas = this.util.merge(this.etiquetas, this.auth.usuario.config?.etiquetas, (a, b) => a.key == b.key);
   }
   loadChecklist() {
     const modeloEntrega = this.entrega?.selectedEntity;
