e3cfe505f18d45605af899d723d3e1423583f017
marco.coelho <marco.coelho@firstbps.com.br>
2024-01-24 23:02:42 -0400

    - Panel - Log - Api
    Signed-off-by: marco.coelho <marco.coelho@firstbps.com.br>

--- back-end/database/migrations/2024_01_23_233114_create_logs_table.php
+++ back-end/database/migrations/2024_01_23_233114_create_logs_table.php
@@ -31,6 +31,6 @@ class CreateLogsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('console_errors');
+        Schema::dropIfExists('logs');
     }
 }
