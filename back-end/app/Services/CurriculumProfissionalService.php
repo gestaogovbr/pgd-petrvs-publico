e5adca35fd3bf83085b964a291cb25429b9a13d8
marcelomaizman <137897604+marcelomaizman@users.noreply.github.com>
2024-03-01 16:01:08 -0300

    Atualizacao RaioX

--- back-end/app/Services/CurriculumProfissionalService.php
+++ back-end/app/Services/CurriculumProfissionalService.php
@@ -4,66 +4,10 @@ namespace App\Services;
 
 use App\Models\CurriculumProfissional;
 use App\Services\ServiceBase;
-use App\Models\Cidade;
-use App\Models\Curriculum;
-use App\Models\Curso;
+
 
 class CurriculumProfissionalService extends ServiceBase
 {
 
-  public function proxyQuery($query, &$data)
-  {
-    $where = [];
-    $usuarios_filtrados = [];
-    foreach ($data['where'] as $condition) {
-      if (is_array($condition) && $condition[0] == 'uf') {
-        $curriculums_filtrados = Curriculum::select('id')->whereRelation('cidade', 'uf', $condition[2])->get()->toArray();
-        array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'cidade_id') {
-        $curriculums_filtrados = Curriculum::select('id')->where('cidade_id', $condition[2])->get()->toArray();
-        array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'estado_civil') {
-        $curriculums_filtrados = Curriculum::select('id')->where('estado_civil', $condition[2])->get()->toArray();
-        array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'filhos') {
-        $tem_filhos = $condition[2];
-        $curriculums_filtrados = [];
-        if ($tem_filhos) {
-          $curriculums_filtrados = Curriculum::select('id')->where('quantidade_filhos', '>', 0)->get()->toArray();
-        } else {
-          $curriculums_filtrados = Curriculum::select('id')->where('quantidade_filhos', 0)->get()->toArray();
-        }
-        array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'idioma') {
-        //$curriculums_filtrados = Curriculum::select('id')->whereRaw("JSON_CONTAINS(idiomas, ?, '$.idioma')", [$condition[2]])->get()->toArray();
-        //array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'area_conhecimento_id') {
-        $cursos_filtrados = Curso::select('id')->where('area_id', $condition[2])->get()->toArray();
-        $curriculums_filtrados = Curriculum::select('id')->whereRelation('graduacoes', fn($q) => $q->whereIn('curso_id', $cursos_filtrados))->get()->toArray();
-        array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'curso_id') {
-        //$curriculums_filtrados = Curriculum::select('id')->where('idiomas', $condition[2])->get()->toArray();
-        //array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'titulo_id') {
-        //$curriculums_filtrados = Curriculum::select('id')->where('idiomas', $condition[2])->get()->toArray();
-        //array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'grupo_especializado_id') {
-        //$curriculums_filtrados = Curriculum::select('id')->where('idiomas', $condition[2])->get()->toArray();
-        //array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'funcao_id') {
-        //$curriculums_filtrados = Curriculum::select('id')->where('idiomas', $condition[2])->get()->toArray();
-        //array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'area_tematica_id') {
-        //$curriculums_filtrados = Curriculum::select('id')->where('idiomas', $condition[2])->get()->toArray();
-        //array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else if (is_array($condition) && $condition[0] == 'capacidade_tecnica_id') {
-        //$curriculums_filtrados = Curriculum::select('id')->where('idiomas', $condition[2])->get()->toArray();
-        //array_push($where, ['curriculum_id', 'in', $curriculums_filtrados]);
-      } else {
-        array_push($where, $condition);
-      }
-    }
-    $data['where'] = $where;
-    return $data;
-  }
+  
 }
--- front-end/src/app/modules/curriculum/curriculum-pesquisa-list/curriculum-pesquisa-list.component.ts
+++ front-end/src/app/modules/curriculum/curriculum-pesquisa-list/curriculum-pesquisa-list.component.ts
@@ -89,22 +89,22 @@ export class CurriculumPesquisaListComponent extends PageListBase<Curriculum, Cu
     if (form.idioma?.length) {
       result.push(["idioma", "==", form.idioma]);
     }
-    if (form.area_conhecimento_id?.length) {
+    if (form.area_conhecimento_id?.length && !form.curso_id?.length) {
       result.push(["area_conhecimento_id", "==", form.area_conhecimento_id]);
     }
     if (form.curso_id?.length) {
       result.push(["curso_id", "==", form.curso_id]);
     }
-    if (form.titulo_id?.length) {
+    /*if (form.titulo_id?.length) {
       result.push(["titulo_id", "==", form.titulo_id]);
-    }
+    }*/
     if (form.grupo_especializado_id?.length) {
       result.push(["grupo_especializado_id", "==", form.grupo_especializado_id]);
     }
     if (form.funcao_id?.length) {
       result.push(["funcao_id", "==", form.funcao_id]);
     }
-    if (form.area_tematica_id?.length) {
+    if (form.area_tematica_id?.length && !form.capacidade_tecnica_id?.length) {
       result.push(["area_tematica_id", "==", form.area_tematica_id]);
     }
     if (form.capacidade_tecnica_id?.length) {
@@ -143,11 +143,11 @@ export class CurriculumPesquisaListComponent extends PageListBase<Curriculum, Cu
     this.cdRef.detectChanges();
   }
 
-  public onTituloChange() {
+  /*public onTituloChange() {
     let titulo = this.lookup.TITULOS_CURSOS.find(x => x.key == this.filter!.controls.titulo_id.value);
     this.cursoWhere = [['area_id', '==', this.filter!.controls.area_conhecimento_id.value], ['titulo', '==', titulo?.key]];
     this.cdRef.detectChanges();
-  }
+  }*/
 
   public dynamicButtons(row: any): ToolbarButton[] {
     const btns = [];
