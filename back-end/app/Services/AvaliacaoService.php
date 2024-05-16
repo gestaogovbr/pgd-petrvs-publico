3359124b9c7882266d4d98e7f693b196d1b4b8b4
Geisimar Rech <geisimar.rech@gestao.gov.br>
2024-05-16 14:19:08 -0400

    validacao para não incluir mais de 1 recurso da avaliação.

--- back-end/app/Services/AvaliacaoService.php
+++ back-end/app/Services/AvaliacaoService.php
@@ -13,51 +13,62 @@ use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Throwable;
 
-class AvaliacaoService extends ServiceBase {
+class AvaliacaoService extends ServiceBase
+{
 
-    public function proxyStore(&$avaliacao, $unidade, $action){
-        $avaliacao["data_avaliacao"] = $this->unidadeService->hora($unidade?->id);
-        $avaliacao["avaliador_id"] = $this->usuarioService->loggedUser()->id;
-        $avaliacao["recurso"] = null;
-        return $avaliacao;
+  public function proxyStore(&$avaliacao, $unidade, $action)
+  {
+    $avaliacao["data_avaliacao"] = $this->unidadeService->hora($unidade?->id);
+    $avaliacao["avaliador_id"] = $this->usuarioService->loggedUser()->id;
+    $avaliacao["recurso"] = null;
+    return $avaliacao;
+  }
+
+  public function extraStore($avaliacao, $unidade, $action)
+  {
+    /* (RN_AVL_5) [PT] Ao realizar qualquer avaliação o status da consolidação deverá ir para AVALIADO; */
+    if (!empty($avaliacao->plano_trabalho_consolidacao_id)) {
+      $this->planoTrabalhoConsolidacaoService->avaliar($avaliacao);
+    } else if (!empty($avaliacao->plano_entrega_id)) {
+      $this->planoEntregaService->avaliar($avaliacao);
     }
+  }
 
-    public function extraStore($avaliacao, $unidade, $action)
-    {
-      /* (RN_AVL_5) [PT] Ao realizar qualquer avaliação o status da consolidação deverá ir para AVALIADO; */
-      if(!empty($avaliacao->plano_trabalho_consolidacao_id)) {
-        $this->planoTrabalhoConsolidacaoService->avaliar($avaliacao);
-      } else if(!empty($avaliacao->plano_entrega_id)) {
-        $this->planoEntregaService->avaliar($avaliacao);
+  public function recorrer($id, $recurso)
+  {
+    $avaliacao = Avaliacao::find($id);
+    $outrasAvaliacoesComRecurso = Avaliacao::where('plano_trabalho_consolidacao_id', $avaliacao->plano_trabalho_consolidacao_id)
+      ->where('id', '!=', $id)
+      ->whereNotNull('recurso')
+      ->get();
+    if ($outrasAvaliacoesComRecurso->count() > 0) {
+      throw new ServerException("ValidateRecursoAvaliacao", "Só é possível recorrer uma vez por avaliação.");
+    }
+    $avaliacao->recurso = $recurso;
+    $avaliacao->save();
+    return true;
+  }
+
+  public function cancelarAvaliacao($id)
+  {
+    $avaliacao = Avaliacao::find($id);
+    $consolidacao = !empty($avaliacao->plano_trabalho_consolidacao_id) ? PlanoTrabalhoConsolidacao::find($avaliacao->plano_trabalho_consolidacao_id) : null;
+    $planoEntrega = !empty($avaliacao->plano_entrega_id) ? PlanoEntrega::find($avaliacao->plano_entrega_id) : null;
+    try {
+      DB::beginTransaction();
+      if (!empty($consolidacao)) {
+        $consolidacao->avaliacao_id = null;
+        $consolidacao->save();
+        $this->statusService->atualizaStatus($consolidacao, 'CONCLUIDO');
+      } else if (!empty($planoEntrega)) {
+        $planoEntrega->avaliacao_id = null;
+        $planoEntrega->save();
+        $this->statusService->atualizaStatus($planoEntrega, 'CONCLUIDO');
       }
-    }    
-
-    public function recorrer($id, $recurso) {
-        $avaliacao = Avaliacao::find($id);
-        $avaliacao->recurso = $recurso;
-        $avaliacao->save();
-        return true;
+      DB::commit();
+    } catch (Throwable $e) {
+      DB::rollback();
+      throw $e;
     }
-
-    public function cancelarAvaliacao($id) {
-        $avaliacao = Avaliacao::find($id);
-        $consolidacao = !empty($avaliacao->plano_trabalho_consolidacao_id) ? PlanoTrabalhoConsolidacao::find($avaliacao->plano_trabalho_consolidacao_id) : null;
-        $planoEntrega = !empty($avaliacao->plano_entrega_id) ? PlanoEntrega::find($avaliacao->plano_entrega_id) : null;
-        try {
-            DB::beginTransaction();
-            if(!empty($consolidacao)) {
-                $consolidacao->avaliacao_id = null;
-                $consolidacao->save();
-                $this->statusService->atualizaStatus($consolidacao, 'CONCLUIDO');
-            } else if(!empty($planoEntrega)) {
-                $planoEntrega->avaliacao_id = null;
-                $planoEntrega->save();
-                $this->statusService->atualizaStatus($planoEntrega, 'CONCLUIDO');
-            }
-            DB::commit();
-        } catch (Throwable $e) {
-            DB::rollback();
-            throw $e;
-        }
-    }
+  }
 }
