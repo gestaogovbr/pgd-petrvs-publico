284df4a2bcc7e94b5ccd49177a22671c3dfa5704
Geisimar Rech <geisimar.rech@gestao.gov.br>
2024-05-21 09:00:06 -0400

    build após atualização

---
+++ back-end/public/browser/chunk-BK3LQZ2W.js
@@ -0,0 +1,1 @@
+import{pc as t}from"./chunk-SDCVQHKS.js";var s=class extends t{constructor(){super(),this.user_id="",this.date_time="",this.table_name="",this.row_id="",this.type="",this.delta=[]}};export{s as a};
