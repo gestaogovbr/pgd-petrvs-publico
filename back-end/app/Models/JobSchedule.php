45f4ef251425a9b81428515efd4cecf6248f944d
diego.braga <diego.braga@firstbps.com.br>
2024-05-29 00:17:37 -0300

    reestruturando agendamento do siape

--- back-end/app/Models/JobSchedule.php
+++ back-end/app/Models/JobSchedule.php
@@ -5,13 +5,14 @@ namespace App\Models;
 use App\Models\ModelBase;
 use App\Traits\AutoUuid;
 
-class JobAgendado extends ModelBase
+class JobSchedule extends ModelBase
 {
     use AutoUuid;
 
     protected $table = 'jobs_schedules';
-    protected $fillable = ['nome_do_job', 'diario', 'horario', 'expressao_cron', 'ativo', 'tenant_id','parameters'];
+    protected $fillable = ['nome', 'classe','minutos', 'horas', 'dias', 'semanas', 'meses', 'expressao_cron', 'ativo', 'tenant_id', 'parameters'];
     protected $casts = [
         'parameters' => 'array'
     ];
+
 }
