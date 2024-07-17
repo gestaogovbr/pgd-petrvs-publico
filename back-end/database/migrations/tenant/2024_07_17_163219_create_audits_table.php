cf7cd4ac775ed1ba89d91f0a9d5e244197b0b2d5
MarcoCoelhoFirstMGI <marco.coelho@firstbps.com.br>
2024-07-17 16:26:55 -0400

    Instalação Audit
    Signed-off-by: MarcoCoelhoFirstMGI <marco.coelho@firstbps.com.br>

---
+++ back-end/database/migrations/tenant/2024_07_17_163219_create_audits_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuditsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $connection = config('audit.drivers.database.connection', config('database.default'));
+        $table = config('audit.drivers.database.table', 'audits');
+
+        Schema::connection($connection)->create($table, function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->string('user_type')->nullable();
+            $table->uuid('user_id')->nullable();
+            $table->string('event');
+            $table->string('auditable_type');
+            $table->uuid('auditable_id');
+            $table->json('old_values')->nullable();
+            $table->json('new_values')->nullable();
+            $table->text('url')->nullable();
+            $table->ipAddress('ip_address')->nullable();
+            $table->string('user_agent', 1023)->nullable();
+            $table->string('tags')->nullable();
+            $table->timestamps();
+
+            $table->index([
+                'user_type',
+                'user_id',
+            ]);
+            $table->index([
+                'auditable_type',
+                'auditable_id',
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $connection = config('audit.drivers.database.connection', config('database.default'));
+        $table = config('audit.drivers.database.table', 'audits');
+
+        Schema::connection($connection)->drop($table);
+    }
+}
