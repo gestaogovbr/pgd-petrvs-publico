f7158d57269f2ffbc5121382f39f83287a9b9eaf
MarcoCoelhoFirstMGI <marco.coelho@firstbps.com.br>
2024-07-26 10:35:20 -0400

    Instalação Audit - View
    Signed-off-by: MarcoCoelhoFirstMGI <marco.coelho@firstbps.com.br>

--- back-end/app/Models/ViewApiPgd.php
+++ back-end/app/Models/ViewApiPgd.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ViewApiPgd extends Model
+class ViewApiPgd extends ModelBase
 {
     public $timestamps = false;
 
