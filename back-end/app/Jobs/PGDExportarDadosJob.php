a27e05acf303235cd5c5db9a5eb56edf80678f09
Ricardo Grana <ricardo.lima@firstbps.com.br>
2024-08-09 03:23:30 -0400

    Adaptação do Job de Envio ao PGD para utilizar sua própria classe de Job

--- back-end/app/Jobs/PGDExportarDadosJob.php
+++ back-end/app/Jobs/PGDExportarDadosJob.php
@@ -2,22 +2,15 @@
 
 namespace App\Jobs;
 
+use App\Exceptions\LogError;
+use Exception;
 use App\Jobs\Contratos\ContratoJobSchedule;
 use App\Models\Tenant;
 use App\Services\API_PGD\Export\ExportarTenantService;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-
-class PGDExportarDadosJob implements ShouldQueue, ShouldBeUnique, ContratoJobSchedule
+class PGDExportarDadosJob extends JobWithoutTenant implements ContratoJobSchedule
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
     public static function getDescricao(): string
     {
         return "Envia Dados para API do PGD";
@@ -25,10 +18,21 @@ class PGDExportarDadosJob implements ShouldQueue, ShouldBeUnique, ContratoJobSch
 
     public function handle(ExportarTenantService $exportarTenantService)
     {
-        Log::alert("PGDExportarDadosJob");
+        try{
+            Log::alert("PGDExportarDadosJob");
 
-        foreach(Tenant::all() as $tenant) {
-            $exportarTenantService->exportar($tenant->id);
+            foreach(Tenant::all() as $tenant) {
+                $exportarTenantService->exportar($tenant->id);
+            }
+        } catch (Exception $e) {
+            $tenant = tenancy()->find($tenant->id);
+            tenancy()->initialize($tenant);
+
+            Log::error("Erro ao processar PGDExportarDadosJob: - Erro: " . $e->getMessage());
+            LogError::newWarn("Erro ao processar PGDExportarDadosJob - Erro: " . $e->getMessage());
+
+            tenancy()->end();
+            return false; 
         }
     }
 
