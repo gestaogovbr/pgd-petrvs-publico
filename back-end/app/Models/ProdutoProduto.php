8c27c4d43ea023b8d3ac1e7212b936f3dccde519
diego.braga <diego.braga@firstbps.com.br>
2024-08-22 01:05:05 -0300

    criando fluxo do produto

---
+++ back-end/app/Models/ProdutoProduto.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProdutoProduto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'produto_produto';
+
+    protected $keyType = 'string';
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'produto_base_id',
+        'produto_id',
+    ];
+
+    
+    public function produtoBase()
+    {
+        return $this->belongsTo(Produto::class, 'produto_base_id');
+    }
+
+    public function produtoRelacionado()
+    {
+        return $this->belongsTo(Produto::class, 'produto_id');
+    }
+}
