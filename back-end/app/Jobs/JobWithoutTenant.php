40d015382034d2df2c7d9a23198eaf2c3f02f809
Ricardo Grana <ricardo.lima@firstbps.com.br>
2024-10-23 11:36:45 -0400

    Adicionado metodo failed ao JobWithouTenant

--- back-end/app/Jobs/JobWithoutTenant.php
+++ back-end/app/Jobs/JobWithoutTenant.php
@@ -9,6 +9,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class JobWithoutTenant implements ShouldQueue
 {
@@ -25,5 +27,10 @@ class JobWithoutTenant implements ShouldQueue
 
         $jobClass = app($fullClassName);
         return new $jobClass();
+    }
+
+    public function failed(?Throwable $exception): void
+    {
+        Log::error("Falha ao executar Job: ".$exception->getMessage());
     }
 }
