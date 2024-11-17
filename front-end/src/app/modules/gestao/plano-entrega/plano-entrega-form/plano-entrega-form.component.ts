9b197e75a9314327dba7d6ed1d5117747a7ddf99
Ricardo Grana <ricardo.lima@firstbps.com.br>
2024-11-17 03:12:06 -0400

    Correção na ordem de seleção dos regramentos

--- front-end/src/app/modules/gestao/plano-entrega/plano-entrega-form/plano-entrega-form.component.ts
+++ front-end/src/app/modules/gestao/plano-entrega/plano-entrega-form/plano-entrega-form.component.ts
@@ -135,7 +135,10 @@ export class PlanoEntregaFormComponent extends PageFormBase<PlanoEntrega, PlanoE
     this.entity.unidade_id = this.auth.unidade?.id || "";
     this.entity.unidade = this.auth.unidade;
 
-    let programas = await this.programaDao.query({where: [['vigentesUnidadeExecutora', '==', this.auth.unidade!.id]]}).asPromise();
+    let programas = await this.programaDao.query({
+      where: [['vigentesUnidadeExecutora', '==', this.auth.unidade!.id]],
+      orderBy: [["unidade.path", "desc"]]
+    }).asPromise();
     let ultimo = programas[0];
     if(ultimo){
       this.entity.programa = ultimo;
