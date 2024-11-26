51c57045f16f7ab755a1b6c8d599c6596738a6e1
Geisimar Rech <geisimar.rech@gestao.gov.br>
2024-11-26 11:29:47 -0400

    Adicionado contador do total de pessoas em PGD

--- front-end/src/app/modules/panel/panel-list/panel-list.component.ts
+++ front-end/src/app/modules/panel/panel-list/panel-list.component.ts
@@ -17,6 +17,7 @@ export class PanelListComponent extends PageListBase<Tenant, TenantDaoService> {
 
 	public currentUser: any;
 	public tenants: Tenant[] = [];
+	public countUsersInPGD: number = 0;
 
 	public toolbarButtons: ToolbarButton[] = [
 		{
@@ -123,6 +124,7 @@ export class PanelListComponent extends PageListBase<Tenant, TenantDaoService> {
 
 	async onLoad() {
 		await this.loadTenants();
+		await this.UsersInPGD();
 		super.onLoad();
 	}
 
@@ -155,6 +157,12 @@ export class PanelListComponent extends PageListBase<Tenant, TenantDaoService> {
 				.catch((error) => {
 					reject(error);
 				});
+		});
+	}
+
+	private UsersInPGD(): Promise<void> {
+		return this.dao!.countUsersInPGD().then((count) => {
+			this.countUsersInPGD = count;
 		});
 	}
 
