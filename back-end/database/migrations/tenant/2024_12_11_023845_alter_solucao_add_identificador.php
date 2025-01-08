f337508b1b020332d7567b74288c5a1ad0713b3f
RIcardo Grana <ricardo.lima@firstbps.com.br>
2025-01-07 22:20:40 -0400

    Correção na coluna de identificador da tabela de solucao

--- back-end/database/migrations/tenant/2024_12_11_023845_alter_solucao_add_identificador.php
+++ back-end/database/migrations/tenant/2024_12_11_023845_alter_solucao_add_identificador.php
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
        Schema::table('solucao_produtos_servicos', function (Blueprint $table) {            
-            $table->bigInteger('identificador')->unsigned()->nullable(true);
+            $table->bigInteger('identificador')->unsigned()->nullable(true)->change();
             $table->unique(['identificador']);
         });
 
