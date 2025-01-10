4f8c981b96762baed40ec15b073632a58f95ff81
diego.braga <diego.braga@firstbps.com.br>
2025-01-10 11:41:09 -0300

    refatorando parta de visualizacao do produto

--- front-end/src/app/modules/gestao/produto/produto-show/produto-show.component.ts
+++ front-end/src/app/modules/gestao/produto/produto-show/produto-show.component.ts
@@ -25,7 +25,7 @@ export class ProdutoShowComponent extends PageBase {
   }
 
   public async carregaProdutoDetalhado(){
-    this.produto = await this.produtoDaoService.getById(this.metadata.produto.id, ["produtoProcessoCadeiaValor.cadeiaValorProcesso.cadeiaValor.unidade", "produtoProduto.produtoRelacionado.unidade"]);
+    this.produto = await this.produtoDaoService.getById(this.metadata.produto.id, ["produtoProcessoCadeiaValor.cadeiaValorProcesso.cadeiaValor.unidade", "produtoProduto.produtoRelacionado.unidade","responsavel", "unidade"]);
     this.loading = false;
   }
 }
