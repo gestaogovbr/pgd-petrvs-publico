83e5b5aa401e231eb6f9d0226cf72ac549f672ce
Geisimar Rech <geisimar.rech@gestao.gov.br>
2025-01-27 09:54:38 -0400

    Adiciona novos perfis de acesso e atualiza a lógica de recuperação de perfil

--- back-end/app/Services/NivelAcessoService.php
+++ back-end/app/Services/NivelAcessoService.php
@@ -9,20 +9,31 @@ class NivelAcessoService extends ServiceBase{
   
   const PERFIL_DESENVOLVEDOR = 0;
   const PERFIL_ADMINISTRADOR = 1;
+
+  const PERFIL_ADMINISTRADOR_NEGOCIAL = 2;
   const PERFIL_CHEFIA = 3;
   const PERFIL_PARTICIPANTE = 5;
+  const PERFIL_COLABORADOR = 6;
 
 
   static function getPerfilDesenvolvedor(): ?Perfil {
       return Perfil::where('nivel', self::PERFIL_DESENVOLVEDOR)->first();
   }
+
+  static function getPerfilAdministradorGeral(): ?Perfil {
+    return Perfil::where('nivel', self::PERFIL_ADMINISTRADOR)->first();
+  }
   static function getPerfilChefia(): ?Perfil {
     return Perfil::where('nivel', self::PERFIL_CHEFIA)->first();
   }
   static function getPerfilAdministrador(): ?Perfil {
-    return Perfil::where('nivel', self::PERFIL_ADMINISTRADOR)->first();
+    return Perfil::where('nivel', self::PERFIL_ADMINISTRADOR_NEGOCIAL)->first();
   }
   static function getPerfilParticipante(): ?Perfil {
     return Perfil::where('nivel', self::PERFIL_PARTICIPANTE)->first();
+  }
+
+  static function getPerfilColaborador(): ?Perfil {
+    return Perfil::where('nivel', self::PERFIL_COLABORADOR)->first();
   }
 }
