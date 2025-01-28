5c29530d2e63be396c96892b92fe94316e0a6655
diego.braga <diego.braga@firstbps.com.br>
2025-01-28 18:39:43 -0300

    atualizando migrata

--- back-end/database/migrations/tenant/2025_01_22_152917_remove_unidades_raizes_duplicadas.php
+++ back-end/database/migrations/tenant/2025_01_22_152917_remove_unidades_raizes_duplicadas.php
@@ -15,6 +15,7 @@ use App\Models\Unidade;
 use App\Models\UnidadeIntegrante;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 
@@ -26,76 +27,110 @@ return new class extends Migration
     public function up(): void
     {
         $unidades = Unidade::join('integracao_unidades as iu', 'unidades.codigo', '=', 'iu.codigo_siape')
-    ->where('iu.pai_siape', '999999')
-    ->select('unidades.*')
-    ->get();
+            ->where('iu.pai_siape', '999999')
+            ->select('unidades.*')
+            ->get();
 
-    $totalRegistros = $unidades->count();
-    
-    if($totalRegistros < 2){
-        return ;
-    }
-    $instituidoraTrue = $unidades->filter(function ($item) {
-        return $item->instituidora == 1;
-    });
-    
-    $instituidoraFalse = $unidades->filter(function ($item) {
-        return $item->instituidora == 0;
-    });
+        $totalRegistros = $unidades->count();
 
-    $idsInstituidoraTrue = $instituidoraTrue->pluck('id');
-    $idsInstituidoraFalse = $instituidoraFalse->pluck('id');
+        if ($totalRegistros < 2) {
+            return;
+        }
+        $instituidoraTrue = $unidades->filter(function ($item) {
+            return $item->instituidora == 1;
+        });
 
-    if ($idsInstituidoraTrue->count() > 1) {
-        $ultimoId = $idsInstituidoraTrue->pop(); 
-        $excedentes = $idsInstituidoraTrue; 
-    
-        $idsInstituidoraTrue = collect([$ultimoId]);
-    
-        $idsInstituidoraFalse = $idsInstituidoraFalse->merge($excedentes);
-    }
+        $instituidoraFalse = $unidades->filter(function ($item) {
+            return $item->instituidora == 0;
+        });
 
-    $idInstituidora = $idsInstituidoraTrue->first();
+        $idsInstituidoraTrue = $instituidoraTrue->pluck('id');
+        $idsInstituidoraFalse = $instituidoraFalse->pluck('id');
+
+        if ($idsInstituidoraTrue->count() > 1) {
+            $ultimoId = $idsInstituidoraTrue->pop();
+            $excedentes = $idsInstituidoraTrue;
+
+            $idsInstituidoraTrue = collect([$ultimoId]);
+
+            $idsInstituidoraFalse = $idsInstituidoraFalse->merge($excedentes);
+        }
+
+        $idInstituidora = $idsInstituidoraTrue->first();
 
 
-    UnidadeIntegrante::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
+        UnidadeIntegrante::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
 
-    Entrega::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
-    
-    PlanoTrabalho::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
+        Entrega::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
 
-    Planejamento::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
+        PlanoTrabalho::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
 
-    Template::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
+        Planejamento::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
 
-    Comparecimento::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
+        Template::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
 
-    HistoricoLotacao::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
+        Comparecimento::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
 
-    ProjetoRecurso::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
+        HistoricoLotacao::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
 
-    PlanoEntregaEntrega::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
-    
-    Programa::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
+        ProjetoRecurso::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
 
-    CadeiaValor::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
+        PlanoEntregaEntrega::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
 
-    PlanoEntrega::whereIn('unidade_id', $idsInstituidoraFalse->toArray())
-    ->update(['unidade_id' => $idInstituidora]);
+        Programa::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
 
-    Unidade::whereIn('id', $idsInstituidoraFalse->toArray())->forceDelete();
+        Programa::withTrashed()
+            ->whereIn('unidade_autorizadora_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_autorizadora_id' => $idInstituidora]);
 
+        CadeiaValor::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
+
+        PlanoEntrega::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
+
+        PlanoEntrega::withTrashed()
+            ->whereIn('unidade_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_id' => $idInstituidora]);
+
+        Unidade::withTrashed()
+            ->whereIn('unidade_pai_id', $idsInstituidoraFalse->toArray())
+            ->update(['unidade_pai_id' => $idInstituidora]);
+
+
+        foreach ($idsInstituidoraFalse->toArray() as $idInstituidoraFalse) {
+            DB::statement("UPDATE unidades SET path = REPLACE(path, ?, ?) WHERE path LIKE ?", [
+                $idInstituidoraFalse,
+                $idInstituidora,
+                "%$idInstituidoraFalse%",
+            ]);
+           
+        }
+
+       
+        Unidade::whereIn('id', $idsInstituidoraFalse->toArray())->forceDelete();
     }
 
     /**
@@ -103,6 +138,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        return ;
+        return;
     }
 };
