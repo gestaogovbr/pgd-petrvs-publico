3716f4e32c96d74a68eb36d5d37f764e8e242d34
Geisimar Rech <geisimar.rech@gestao.gov.br>
2025-03-06 13:18:55 -0400

    Refactor ProdutoListClienteComponent to unify form variable naming and improve code consistency

--- front-end/src/app/modules/gestao/produto/produto-list-cliente/produto-list-cliente.component.ts
+++ front-end/src/app/modules/gestao/produto/produto-list-cliente/produto-list-cliente.component.ts
@@ -46,7 +46,6 @@ export class ProdutoListClienteComponent extends PageFrameBase {
   }
 
   private _disabled: boolean = false;
-  public form_cliente: FormGroup;
 
   constructor(public injector: Injector) {
     super(injector);
@@ -57,7 +56,7 @@ export class ProdutoListClienteComponent extends PageFrameBase {
     this.unidadeDao = injector.get<UnidadeDaoService>(UnidadeDaoService);
     this.cdRef = injector.get<ChangeDetectorRef>(ChangeDetectorRef);
 
-    this.form_cliente = this.fh.FormBuilder({
+    this.form = this.fh.FormBuilder({
       cliente_id: { default: "" },
       tipo_cliente_id: { default: "" },
       unidade_relacionada_id: { default: "" },
@@ -107,12 +106,12 @@ export class ProdutoListClienteComponent extends PageFrameBase {
   public async saveCliente(form: FormGroup, row: any) { 
     this.entity!._metadata = this.entity!._metadata || {};
     this.entity!._metadata.produtoCliente = row as ProdutoCliente;
-    if(this.form_cliente!.valid) {     
+    if(this.form!.valid) {     
       row.id = row.id == "NEW" ? this.dao!.generateUuid() : row.id;
 
       // se o tipo cliente for diferente de Administração Pública e Público externo
       if(this.tipoCliente?.selectedItem && ['Administração Pública', 'Público externo'].includes(this.tipoCliente.selectedItem.value)) {
-        row.cliente_id = this.form_cliente!.controls.cliente_id.value;
+        row.cliente_id = this.form!.controls.cliente_id.value;
         row.cliente = {
           ...this.cliente!.selectedItem,
           tipo_cliente_id: this.tipoCliente!.selectedItem!.key,
@@ -123,7 +122,7 @@ export class ProdutoListClienteComponent extends PageFrameBase {
         };
         
       } else if(this.tipoCliente?.selectedItem?.value == 'Outros') {
-        const queryResult = await this.clienteDao?.query({ where: [['nome', '==', this.form_cliente!.controls.outro.value]], join: ['tipoCliente'] }).asPromise();
+        const queryResult = await this.clienteDao?.query({ where: [['nome', '==', this.form!.controls.outro.value]], join: ['tipoCliente'] }).asPromise();
         const cliente: Cliente[] = queryResult ? queryResult as unknown as Cliente[] : [];
         if(cliente && cliente.length > 0) {
           row.cliente_id = cliente[0].id;
@@ -131,7 +130,7 @@ export class ProdutoListClienteComponent extends PageFrameBase {
         } else {
           const novoCliente = new Cliente();
           novoCliente.id = this.clienteDao!.generateUuid();
-          novoCliente.nome = this.form_cliente!.controls.outro.value;
+          novoCliente.nome = this.form!.controls.outro.value;
           novoCliente.tipo_cliente_id = this.tipoCliente?.selectedItem?.key;
           const cliente = await this.clienteDao?.save(novoCliente, ['tipoCliente']);
           if (cliente) {   
@@ -141,7 +140,7 @@ export class ProdutoListClienteComponent extends PageFrameBase {
           }
         } 
       } else if(this.tipoCliente?.selectedItem?.value == 'Unidade de órgão/entidade') {
-        const queryResult = await this.clienteDao?.query({ where: [['unidade_id', '==', this.form_cliente!.controls.unidade_relacionada_id.value]], join: ['tipoCliente'] }).asPromise();
+        const queryResult = await this.clienteDao?.query({ where: [['unidade_id', '==', this.form!.controls.unidade_relacionada_id.value]], join: ['tipoCliente'] }).asPromise();
         const cliente: Cliente[] = queryResult ? queryResult as unknown as Cliente[] : [];
         if(cliente && cliente.length > 0) {
           row.cliente_id = cliente[0].id;
@@ -149,7 +148,7 @@ export class ProdutoListClienteComponent extends PageFrameBase {
         } else {
           const novoCliente = new Cliente();
           novoCliente.id = this.clienteDao!.generateUuid();
-          novoCliente.unidade_id = this.form_cliente!.controls.unidade_relacionada_id.value;
+          novoCliente.unidade_id = this.form!.controls.unidade_relacionada_id.value;
           novoCliente.nome = this.unidade_relacionada?.selectedEntity?.nome;
           novoCliente.tipo_cliente_id = this.tipoCliente?.selectedItem?.key;
           const cliente = await this.clienteDao?.save(novoCliente, ['tipoCliente']);
@@ -169,9 +168,6 @@ export class ProdutoListClienteComponent extends PageFrameBase {
     return this.entity!._metadata.produtoCliente;
   }
 
-  changeTipoCliente() {
-    console.log(this.tipoCliente?.selectedItem);
-    
-  }
+ 
 
 }
