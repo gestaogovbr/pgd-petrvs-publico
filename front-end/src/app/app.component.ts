9a5ac48a905d8ad2d3721eab473e0747617ea2c8
Ricardo Grana <ricardo.lima@firstbps.com.br>
2025-04-03 11:14:21 -0400

    Revert "Ocultados menus de Produtos"
    This reverts commit 83cc4a510cc0efdc2615c9a7e86b45b113e5726f.

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -196,8 +196,8 @@ export class AppComponent {
       menu: [
         this.menuSchema.PLANEJAMENTOS_INSTITUCIONAIS,
         this.menuSchema.CADEIAS_VALORES,
-        // this.menuSchema.SOLUCOES,
-        // this.menuSchema.PRODUTOS,
+        this.menuSchema.SOLUCOES,
+        this.menuSchema.PRODUTOS,
         this.menuSchema.PROGRAMAS_GESTAO,
         this.menuSchema.HABILITACOES_PROGRAMA,
         this.menuSchema.PLANOS_ENTREGAS,
