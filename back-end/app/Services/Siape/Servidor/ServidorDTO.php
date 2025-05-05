e3662e75a421ce06126f64ad13540bffe181c119
diego.braga <diego.braga@firstbps.com.br>
2025-05-05 19:34:37 -0300

    removendo jornada no back end

--- back-end/app/Services/Siape/Servidor/ServidorDTO.php
+++ back-end/app/Services/Siape/Servidor/ServidorDTO.php
@@ -29,8 +29,6 @@ class ServidorDTO
     public ?string $funcoes;
     public ?string $cpf_chefia_imediata;
     public ?string $email_chefia_imediata;
-    public ?string $nome_jornada;
-    public ?int $cod_jornada;
     public ?string $deleted_at;
 
     public function __construct(array $data)
