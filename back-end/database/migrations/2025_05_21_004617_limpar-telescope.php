f4723312dba1265d3744d2b40bad4802ddbaea15
Ricardo Grana <ricardo.lima@firstbps.com.br>
2025-05-20 23:58:26 -0400

    Adicionada migration para limpar telescope

---
+++ back-end/database/migrations/2025_05_21_004617_limpar-telescope.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('telescope_entries_tags')->delete();
+        DB::table('telescope_entries')->delete();
+        DB::table('telescope_monitoring')->delete();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
