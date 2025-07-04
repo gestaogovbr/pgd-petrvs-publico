094816f4fcce35700f13ba71478a2354789c09f7
Geisimar Rech <geisimar.rech@gestao.gov.br>
2025-07-04 15:51:46 -0400

    Deprecate unidadesIntegrantes method and update atribuições retrieval in UnidadeIntegranteService

--- back-end/app/Models/Usuario.php
+++ back-end/app/Models/Usuario.php
@@ -316,6 +316,10 @@ class Usuario extends Authenticatable implements AuditableContract
         return $this->hasMany(PlanoEntrega::class, 'criacao_usuario_id');
     }
 
+    /**
+     * @deprecated message: Não utilizar essa função, será descontinuada.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function unidadesIntegrantes()
     {
         return $this->hasMany(UnidadeIntegrante::class);
