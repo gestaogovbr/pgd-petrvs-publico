c7ac1c7ac6925269860a6b5b5a2fe3241e3197e2
Ricardo Grana <ricardo.lima@firstbps.com.br>
2025-07-18 15:45:27 -0400

    Ajuste na view do Relatorio de Agentes

--- back-end/database/migrations/tenant/2025_07_14_185237_create-view-relatorio-agentes.php
+++ back-end/database/migrations/tenant/2025_07_14_185237_create-view-relatorio-agentes.php
@@ -14,58 +14,93 @@ return new class extends Migration
          DB::statement(<<<EOD
         CREATE OR REPLACE
         ALGORITHM = UNDEFINED VIEW `view_relatorio_agentes` AS
-        select distinct u.id, u.nome, u.matricula, '' as jornada,
-            p.nome as perfil,
-            u.situacao_funcional,
-            programa_ultimo.programaNome,
-            uni.sigla as unidadeNome,
-            tm.nome as tipoModalidadeNome
-        from usuarios u
+        SELECT distinct `u`.`id` AS `id`,
+            `u`.`nome` AS `nome`,
+            `u`.`matricula` AS `matricula`,
+            '' AS `jornada`,
+            u.perfil_id,
+            `p`.`nome` AS `perfil`,
+            `u`.`situacao_funcional` AS `situacao_funcional`,
+            `programa_ultimo`.`programanome` AS `programaNome`,
+            `uni`.`id` AS `unidade_id`,
+            `uni`.`sigla` AS `unidadeNome`,
+            `fn_obter_unidade_hierarquia`(`uni`.`id`) AS `unidadeHierarquia`,
+            `pt_ultimo_pactuado`.`tipo_modalidade_id` AS `tipo_modalidade_id`,
+            `tm`.`nome` AS `tipoModalidadeNome`,
+            u.data_inicial_pedagio,
+            u.data_final_pedagio,
+            u.tipo_pedagio
+        from
+            (((((((`usuarios` `u`
         left join (
-            select * from (
-                select pp.usuario_id, p.nome as programanome,
-                    row_number() over (
-                        partition by pp.usuario_id
-                        order by pp.created_at desc
-                    ) as rn
-                from programas_participantes pp
-                inner join programas p on p.id = pp.programa_id and p.deleted_at is null
-                where pp.deleted_at is null
-            ) pp1
-            where pp1.rn = 1
-        ) programa_ultimo on programa_ultimo.usuario_id = u.id
+            select
+                `pp1`.`usuario_id` AS `usuario_id`,
+                `pp1`.`programanome` AS `programanome`,
+                `pp1`.`rn` AS `rn`
+            from
+                (
+                select
+                    `pp`.`usuario_id` AS `usuario_id`,
+                    `p`.`nome` AS `programanome`,
+                    row_number() over ( partition by `pp`.`usuario_id`
+                order by
+                    `pp`.`created_at` desc) AS `rn`
+                from
+                    (`programas_participantes` `pp`
+                join `programas` `p` on
+                    (`p`.`id` = `pp`.`programa_id`
+                        and `p`.`deleted_at` is null))
+                where
+                    `pp`.`deleted_at` is null) `pp1`
+            where
+                `pp1`.`rn` = 1) `programa_ultimo` on
+            (`programa_ultimo`.`usuario_id` = `u`.`id`))
         left join (
-            SELECT
-            pt.usuario_id,
-            pt.id AS id,
-            pt.tipo_modalidade_id
-            FROM planos_trabalhos pt
-            WHERE pt.deleted_at IS NULL
-            AND pt.status IN ('ATIVO', 'CONCLUIDO', 'AVALIADO', 'SUSPENSO')
-            AND (pt.data_inicio, pt.id) = (
-                SELECT pt2.data_inicio, MAX(pt2.id)
-                FROM planos_trabalhos pt2
-                WHERE pt2.usuario_id = pt.usuario_id
-                AND pt2.deleted_at IS NULL
-                AND pt2.status IN ('ATIVO', 'CONCLUIDO', 'AVALIADO', 'SUSPENSO')
-                GROUP BY pt2.data_inicio
-                ORDER BY pt2.data_inicio DESC
-                LIMIT 1
-            )
-        ) pt_ultimo_pactuado on pt_ultimo_pactuado.usuario_id = u.id
-        left join unidades_integrantes ui on ui.usuario_id = u.id and ui.deleted_at is null
-        left join unidades_integrantes_atribuicoes uia
-            on uia.unidade_integrante_id = ui.id
-            and uia.deleted_at is null
-            and uia.atribuicao = 'LOTADO'
-        left join unidades uni
-            on uni.codigo = ui.unidade_id
-            and uni.deleted_at is null
-            and uia.atribuicao = 'LOTADO'
-        inner join perfis p on p.id = u.perfil_id
-        inner join tipos_modalidades tm on tm.id = pt_ultimo_pactuado.tipo_modalidade_id
-        where u.deleted_at is null
-        order by u.nome
+            select
+                `pt`.`usuario_id` AS `usuario_id`,
+                `pt`.`id` AS `id`,
+                `pt`.`tipo_modalidade_id` AS `tipo_modalidade_id`
+            from
+                `planos_trabalhos` `pt`
+            where
+                `pt`.`deleted_at` is null
+                and `pt`.`status` in ('ATIVO', 'CONCLUIDO', 'AVALIADO', 'SUSPENSO')
+                    and (`pt`.`data_inicio`,
+                    `pt`.`id`) = (
+                    select
+                        `pt2`.`data_inicio`,
+                        max(`pt2`.`id`)
+                    from
+                        `planos_trabalhos` `pt2`
+                    where
+                        `pt2`.`usuario_id` = `pt`.`usuario_id`
+                        and `pt2`.`deleted_at` is null
+                        and `pt2`.`status` in ('ATIVO', 'CONCLUIDO', 'AVALIADO', 'SUSPENSO')
+                    group by
+                        `pt2`.`data_inicio`
+                    order by
+                        `pt2`.`data_inicio` desc
+                    limit 1)) `pt_ultimo_pactuado` on
+            (`pt_ultimo_pactuado`.`usuario_id` = `u`.`id`))
+        left join `unidades_integrantes` `ui` on
+            (`ui`.`usuario_id` = `u`.`id`
+                and `ui`.`deleted_at` is null))
+        left join `unidades_integrantes_atribuicoes` `uia` on
+            (`uia`.`unidade_integrante_id` = `ui`.`id`
+                and `uia`.`deleted_at` is null
+                and `uia`.`atribuicao` = 'LOTADO'))
+        left join `unidades` `uni` on
+            (`uni`.`id` = `ui`.`unidade_id`
+                and `uni`.`deleted_at` is null
+                and `uia`.`atribuicao` = 'LOTADO'))
+        left join `perfis` `p` on
+            (`p`.`id` = `u`.`perfil_id`))
+        left join `tipos_modalidades` `tm` on
+            (`tm`.`id` = `pt_ultimo_pactuado`.`tipo_modalidade_id`))
+        where
+            `u`.`deleted_at` is null
+        order by
+            `u`.`nome`
         EOD);
     }
 
