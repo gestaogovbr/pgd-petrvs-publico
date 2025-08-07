257eecc7e67b53fb4e19ed58fddf9ce3243cccaa
Lucasfdesousa <lucas.desousa@dataprev.gov.br>
2025-08-07 15:23:30 -0300

    change 'data_modificacao' to sync siape data

---
+++ back-end/database/migrations/tenant/2025_08_07_152112_change_data_modificacao_integracao_servidores.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::transaction(function () {
+            DB::table('intregacao_servidores')->update(['data_modificacao' => DateTime::createFromFormat('dmY', '01012020')->format('Y-m-d 00:00:00')]);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    }
+};
