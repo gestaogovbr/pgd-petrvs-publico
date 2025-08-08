431bab66e57c05cf3675054a4862efc4af962126
Lucasfdesousa <lucas.desousa@dataprev.gov.br>
2025-08-08 08:49:06 -0300

    fix migration table name to 'integracao_servidores'

--- back-end/database/migrations/tenant/2025_08_07_152112_change_data_modificacao_integracao_servidores.php
+++ back-end/database/migrations/tenant/2025_08_07_152112_change_data_modificacao_integracao_servidores.php
@@ -11,7 +11,7 @@ return new class extends Migration
     public function up(): void
     {
         DB::transaction(function () {
-            DB::table('intregacao_servidores')->update(['data_modificacao' => DateTime::createFromFormat('dmY', '01012020')->format('Y-m-d 00:00:00')]);
+            DB::table('integracao_servidores')->update(['data_modificacao' => DateTime::createFromFormat('dmY', '01012020')->format('Y-m-d 00:00:00')]);
         });
 
     }
