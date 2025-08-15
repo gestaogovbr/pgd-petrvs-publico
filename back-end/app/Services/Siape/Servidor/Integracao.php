91977304456769b052495b9cfa3e978dfcf4c27b
Lucasfdesousa <lucas.desousa@dataprev.gov.br>
2025-08-15 14:15:15 -0300

    update 'moadlidade' attributes in integracao_servidores table.

--- back-end/app/Services/Siape/Servidor/Integracao.php
+++ back-end/app/Services/Siape/Servidor/Integracao.php
@@ -104,6 +104,7 @@ class Integracao implements InterfaceIntegracao
                 'codigo_servo_exercicio', 'nomeguerra', 'codigo_situacao_funcional', 
                 'situacao_funcional', 'codupag', 'dataexercicionoorgao', 'funcoes', 
                 'cpf_chefia_imediata', 'email_chefia_imediata'
+                ,'modalidade_pgd','participa_pgd'
             ]);
             $registro =  $this->repository->update($entidade->cpf, $dadosAtualizados);
             array_push($this->cpfsIntegracaoAlterados, $entidade->cpf);
@@ -165,7 +166,7 @@ class Integracao implements InterfaceIntegracao
             'funcoes' => $ativo['funcoes'],
             'matricula' => $this->utilService->valueOrDefault($ativo['matriculasiape'], null),
             'modalidade_pgd' => $this->utilService->valueOrDefault($servidor['modalidade_pgd'], null),
-            'participa_pgd' => $this->utilService->valueOrDefault($servidor['participa_pgd'], null),
+            'participa_pgd' => $this->utilService->valueOrDefault($servidor['participa_pgd'], 'não'),
             'cpf_chefia_imediata' => $this->getCPFChefiaImediata($servidor, $this->utilService),
             'email_chefia_imediata' => $this->getEmailChefiaImediata($servidor, $this->utilService),
             'deleted_at' => null,
