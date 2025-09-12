07ca684c8660d928e7c41819ec88d80172ae82b8
Ricardo Grana <ricardo.lima@firstbps.com.br>
2025-09-12 00:36:16 -0400

    Adicionada verificação de lotação nas assinaturas do PT

--- front-end/src/app/modules/gestao/plano-trabalho/plano-trabalho-form/plano-trabalho-form.component.ts
+++ front-end/src/app/modules/gestao/plano-trabalho/plano-trabalho-form/plano-trabalho-form.component.ts
@@ -452,16 +452,25 @@ export class PlanoTrabalhoFormComponent extends PageFormBase<PlanoTrabalho, Plan
   }
 
   public exibeAlertaTotalAssinaturas(plano: PlanoTrabalho | undefined) {
-    if(plano){
-       let assinaturasExigidas = plano._metadata?.quantidadeAssinaturasExigidas;
-       let unidadeVinculada = plano._metadata?.unidadeVinculada;
-       let msg = ''
-       if (assinaturasExigidas == 1) 
+    if(plano && plano._metadata){
+      console.log(plano);
+      console.log(plano.unidade_id);
+      console.log(this.auth.usuario?.lotacao?.unidade_id);
+      
+      let assinaturasExigidas = plano._metadata?.quantidadeAssinaturasExigidas;
+      let unidadeVinculada = plano._metadata?.unidadeVinculada;
+      let msg = ''
+      
+      if (assinaturasExigidas == 1) 
         msg = "O participante tem atribuição de chefia substituta da unidade superior à sua unidade de lotação. Por isso, este Plano de Trabalho exigirá somente uma assinatura.";
-       else if (assinaturasExigidas == 3 || !unidadeVinculada)
+      else if ((assinaturasExigidas == 3 || !unidadeVinculada) 
+          && (plano.unidade_id != this.auth.usuario?.lotacao?.unidade_id)) {
         msg = "Este Plano de Trabalho está sendo criado numa unidade diferente da unidade de lotação. Por isso, a chefia da unidade do plano também deverá assiná-lo";
-      if (assinaturasExigidas != 2 || msg !== '')
+      }
+
+      if (assinaturasExigidas != 2 || msg !== '') {
         this.dialog.alert("Atenção", msg, "OK");
+      }
     }
   }
 
