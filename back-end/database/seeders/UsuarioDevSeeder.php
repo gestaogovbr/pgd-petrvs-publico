092bdfb0633f628bffb45ee6322b698ad3136c49
marcocoelho <marco.coelho@firstbps.com.br>
2025-10-06 07:57:10 -0400

    fix: Inclusao User Gabriel como is_admin

---
+++ back-end/database/seeders/UsuarioDevSeeder.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Services\NivelAcessoService;
+use App\Models\Unidade;
+use App\Models\Usuario;
+use App\Models\UnidadeIntegrante;
+use App\Models\UnidadeIntegranteAtribuicao;
+
+class UsuarioDevSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public $timenow;
+  public $nivelAcessoService;
+
+  public function __construct()
+  {
+    $this->timenow = now();
+    $this->nivelAcessoService = new NivelAcessoService();
+  }
+
+  public function run()
+  {
+    $perfilDevId = $this->nivelAcessoService->getPerfilDesenvolvedor()->id;
+    $unidadePaiId = Unidade::where('unidade_pai_id', null)
+      ->first()
+      ->id;
+
+    $usuarios = [
+      [
+        'email' => 'gabriel.s.domingos@gestao.gov.br',
+        'nome' => 'Gabriel Marcos da Silva Domingos',
+        'cpf' => '00000000000',
+        'apelido' => 'Gabriel',
+        'perfil_id' => $perfilDevId,
+        'sexo' => 'MASCULINO',
+        'is_admin' => true,
+      ],
+    ];
+
+    foreach ($usuarios as $usuarioData) {
+      $usuario = Usuario::where('email', $usuarioData['email'])->first();
+
+      if ($usuario) {
+        if (!$usuario->is_admin) {
+          $usuario->is_admin = true;
+          $usuario->updated_at = $this->timenow;
+          $usuario->save();
+        }
+        continue;
+      }
+
+      $usuario = Usuario::create([
+        'email' => $usuarioData['email'],
+        'nome' => $usuarioData['nome'],
+        'cpf' => $usuarioData['cpf'],
+        'apelido' => $usuarioData['apelido'],
+        'perfil_id' => $usuarioData['perfil_id'],
+        'sexo' => $usuarioData['sexo'],
+        'is_admin' => $usuarioData['is_admin'],
+        'created_at' => $this->timenow,
+        'updated_at' => $this->timenow,
+      ]);
+
+      $integrante = UnidadeIntegrante::firstOrCreate([
+        'unidade_id' => $unidadePaiId,
+        'usuario_id' => $usuario->id,
+      ]);
+
+      UnidadeIntegranteAtribuicao::firstOrCreate([
+        'atribuicao' => 'LOTADO',
+        'unidade_integrante_id' => $integrante->id,
+      ]);
+    }
+  }
+}
+
+
