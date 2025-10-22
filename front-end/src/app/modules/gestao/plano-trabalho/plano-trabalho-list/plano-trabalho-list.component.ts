07bcc32d54e9b9bb05249758d8f9d830452a2663
Lucasfdesousa <lucas.desousa@dataprev.gov.br>
2025-10-22 15:49:42 -0300

    Enable cloning of PE/PT for statuses except 'Aguardando Assinatura/Homologação'

--- front-end/src/app/modules/gestao/plano-trabalho/plano-trabalho-list/plano-trabalho-list.component.ts
+++ front-end/src/app/modules/gestao/plano-trabalho/plano-trabalho-list/plano-trabalho-list.component.ts
@@ -753,7 +753,7 @@ export class PlanoTrabalhoListComponent extends PageListBase<
 					case this.OPTION_LOGS:
 						return true;
 					case this.BOTAO_CLONAR:
-						return (planoConcluido || planoAvaliado) && this.auth.hasPermissionTo("MOD_PTR_INCL");
+						return (!planoAguardandoAssinatura) && this.auth.hasPermissionTo("MOD_PTR_INCL");
 				}
 			}
 		}
