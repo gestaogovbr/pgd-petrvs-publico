dc01fc375ab72929bb6aeec7c2aa64a20d930132
Ricardo Grana <ricardo.lima@firstbps.com.br>
2025-11-13 00:12:51 -0400

    Revert "feat(notifications): add Microsoft Teams notification channel"
    This reverts commit 3767cf0c6ff25b66b9516b947a53fa12ce5a758a.

--- back-end/config/app.php
+++ back-end/config/app.php
@@ -189,7 +189,6 @@ return [
         App\Providers\UtilServiceProvider::class,
         Lab404\Impersonate\ImpersonateServiceProvider::class,
         OwenIt\Auditing\AuditingServiceProvider::class,
-        NotificationChannels\MicrosoftTeams\MicrosoftTeamsServiceProvider::class,
     ],
 
     /*
