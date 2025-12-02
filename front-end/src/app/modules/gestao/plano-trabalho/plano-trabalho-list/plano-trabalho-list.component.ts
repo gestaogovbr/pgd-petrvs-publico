a7684b90fe18535f886e6291d9ebeb515a096ab3
Geisimar Rech <geisimar.rech@gestao.gov.br>
2025-12-02 18:42:44 -0400

    refactor: simplify proxyRows logic and clean up document display
    - Split query.get() and proxyRows() calls for better readability in ServiceBase
    - Remove unnecessary console.log in PlanoTrabalhoListComponent
    - Clean up document badge template by removing redundant link display logic

--- front-end/src/app/modules/gestao/plano-trabalho/plano-trabalho-list/plano-trabalho-list.component.ts
+++ front-end/src/app/modules/gestao/plano-trabalho/plano-trabalho-list/plano-trabalho-list.component.ts
@@ -123,7 +123,7 @@ export class PlanoTrabalhoListComponent extends PageListBase<
 			"documento.assinaturas.usuario:id,nome,url_foto",
             "documento.assinaturas:id,usuario_id,documento_id",
 			"programa:id,nome",
-            "documento:id,numero",
+            "documento:id,numero,titulo,especie,conteudo,tipo",
 			"tipo_modalidade:id,nome",
 			"entregas.plano_entrega_entrega.entrega",
 			"entregas.plano_entrega_entrega.plano_entrega:id,unidade_id",
@@ -479,9 +479,6 @@ export class PlanoTrabalhoListComponent extends PageListBase<
 		}
 
 		if(this.grid) this.grid.groupBy = this.groupBy;
-
-		console.log(result);
-		
 		return result;
 	};
 
