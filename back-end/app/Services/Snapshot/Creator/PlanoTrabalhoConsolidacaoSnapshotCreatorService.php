f7ace90fa7646261b499f1b4538b2e20c842353c
Leon Siqueira <leon.siqueira@dataprev.gov.br>
2026-01-15 18:31:58 -0300

    wip(refactor): dependency injection

--- back-end/app/Services/Snapshot/Creator/PlanoTrabalhoConsolidacaoSnapshotCreatorService.php
+++ back-end/app/Services/Snapshot/Creator/PlanoTrabalhoConsolidacaoSnapshotCreatorService.php
@@ -7,25 +7,28 @@ class PlanoTrabalhoConsolidacaoSnapshotCreatorService
     /** @var array<string, SnapshotCreatorInterface> */
     protected array $creators = [];
 
-    public function __construct()
+    public function __construct($creators = null)
     {
-        $this->creators = [
-            'atividades' => new AtividadeSnapshotCreator(),
-            'afastamentos' => new AfastamentoSnapshotCreator(),
-            'ocorrencias' => new OcorrenciaSnapshotCreator(),
-        ];
+        // $this->creators = [
+        //     'atividades' => new AtividadeSnapshotCreator(),
+        //     'afastamentos' => new AfastamentoSnapshotCreator(),
+        //     'ocorrencias' => new OcorrenciaSnapshotCreator(),
+        // ];
+
+        foreach ($creators as $key => $creator) {
+            if(!$creator instanceof SnapshotCreatorInterface) throw new \TypeError();
+            $this->creators[$key] = $creator;
+        }
     }
 
     public function createSnapshots(array $dados, string $consolidacaoId, $dataConclusao): void
     {
         /** @var SnapshotCreatorInterface $creator */
         foreach ($this->creators as $key => $creator) {
-            assert($creator instanceof SnapshotCreatorInterface);
-            if (isset($dados[$key])) {
-                $ids = array_map(fn($item) => $item["id"], $dados[$key]);
-                foreach ($ids as $id) {
-                    $creator->create($id, $consolidacaoId, $dataConclusao);
-                }
+            if (!is_array($dados[$key])) throw new \TypeError();
+            $ids = array_map(fn($item) => $item["id"], $dados[$key]);
+            foreach ($ids as $id) {
+                $creator->create($id, $consolidacaoId, $dataConclusao);
             }
         }
     }
--- back-end/app/Services/Snapshot/Rebuilder/PlanoTrabalhoConsolidacaoRebuildService.php
+++ back-end/app/Services/Snapshot/Rebuilder/PlanoTrabalhoConsolidacaoRebuildService.php
@@ -10,23 +10,26 @@ class PlanoTrabalhoConsolidacaoRebuildService
     /** @var array<string, SnapshotRebuilderInterface> */
     private $rebuilders;
 
-    public function __construct()
+    public function __construct($rebuilders = null)
     {
+        // $this->rebuilders = [
+        //     'atividades' => new AtividadeSnapshotCreator(),
+        //     'afastamentos' => new AfastamentoSnapshotCreator(),
+        //     'ocorrencias' => new OcorrenciaSnapshotCreator(),
+        // ];
 
-        $this->rebuilders = [
-            'atividades' => new AtividadeSnapshotRebuilder(),
-            'afastamento' => new AfastamentoSnapshotRebuilder(),
-            'ocorrencia' => new OcorrenciaSnapshotRebuilder(),
-        ];
+        foreach ($rebuilders as $key => $rebuilder) {
+            if(!$rebuilder instanceof BaseRebuilder) throw new \TypeError();
+            $this->rebuilders[$key] = $rebuilder;
+        }
     }
 
     public function rebuildCollections(Collection $collection, PlanoTrabalhoConsolidacao $consolidacao, string $type): array
     {
         $rebuilder = $this->rebuilders[$type] ?? null;
 
-        if (!$rebuilder instanceof BaseRebuilder) {
-            throw new \InvalidArgumentException("Rebuilder não listado: {$type}");
-        }
+        if (!$rebuilder instanceof BaseRebuilder) throw new \TypeError("Rebuilder não listado: {$type}");
+        
 
         return $rebuilder->rebuildCollection($collection, $consolidacao->id, $consolidacao->data_conclusao);
     }
