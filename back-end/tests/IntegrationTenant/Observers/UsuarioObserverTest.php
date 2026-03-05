bc2e859cac98251269253b97471c1868c174764e
Ricardo Grana <ricardo.lima@firstbps.com.br>
2026-03-05 02:10:27 -0400

    Adição de mais testes ao UsuarioObserverTest

--- back-end/tests/IntegrationTenant/Observers/UsuarioObserverTest.php
+++ back-end/tests/IntegrationTenant/Observers/UsuarioObserverTest.php
@@ -3,9 +3,14 @@
 namespace Tests\IntegrationTenant\Observers;
 
 use App\Models\Usuario;
+use App\Repository\IntegracaoServidorRepository;
+use App\Repository\UsuarioRepository;
 use App\Services\API_PGD\UsuarioEnvioService;
+use App\Services\IntegracaoService;
+use App\Services\UsuarioService;
 use Illuminate\Support\Facades\Log;
 use Mockery;
+
 
 afterAll(function () {
     Mockery::close();
@@ -13,7 +18,7 @@ afterAll(function () {
 
 describe('UsuarioObserver', function () {
 
-    it('Usuário Observer é chamado ao atualizar usuário', function () {
+    test('Usuário Observer é chamado ao atualizar usuário', function () {
 
         $serviceMock = Mockery::mock('alias:' . UsuarioEnvioService::class);
         $serviceMock->shouldReceive('processar')
@@ -28,6 +33,53 @@ describe('UsuarioObserver', function () {
             'nome' => 'Usuario Alterado'
         ]);
 
+    });
+
+    test('Executa observer ao executar atualizarServidor', function () {
+        $serviceMock = Mockery::mock('alias:' . UsuarioEnvioService::class);
+
+        $serviceMock->shouldReceive('processar')
+            ->once()
+            ->with(Mockery::any(), Mockery::type(Usuario::class), 'Usuario');
+
+        $usuario = Usuario::factory()->create([
+            'nome' => 'Usuario Teste'
+        ]);
+
+        $usuarioService = new UsuarioService();
+
+        $dto = (object) [
+            'id' => $usuario->id,
+            'matriculasiape' => '123',
+            'nome_servidor' => 'Servidor Teste',
+            'nome_guerra' => 'Teste',
+            'emailfuncional' => 'teste@email.com',
+            'cod_jornada' => 40,
+            'nome_jornada' => 'Integral',
+            'modalidade_pgd' => 1,
+            'participa_pgd' => true,
+            'ident_unica' => 'ABC',
+            'data_modificacao' => now(),
+            'data_nascimento' => '1990-01-01'
+        ];
+
+        $usuarioService->atualizarServidor($dto, $usuario->unidade_id);
+    });
+
+    test('Executa observer ao atualizar pelo UsuarioRepository', function () {
+        $serviceMock = Mockery::mock('alias:' . UsuarioEnvioService::class);
+        $serviceMock->shouldReceive('processar')
+            ->once()
+            ->with(Mockery::any(), Mockery::type(Usuario::class), 'Usuario');
+
+        $usuario = Usuario::factory()->create([
+            'nome' => 'Usuario Teste'
+        ]);
+
+        $matriculaSiape = fake()->numerify('########');
+
+        $usuarioRepository = app(UsuarioRepository::class);
+        $usuarioRepository->update($usuario->id, ['matricula' => $matriculaSiape]);
     });
 
 });
