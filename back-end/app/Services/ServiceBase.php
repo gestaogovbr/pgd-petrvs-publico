316f68a9dc995db85fdbef719fc9dfa245aed76c
Geisimar Rech <geisimar.rech@gestao.gov.br>
2026-03-05 14:58:23 -0300

    fix: disable joinable property to prevent front-end join errors
    The joinable property was causing errors when processing joins from the front-end. This change comments out the property to prevent these errors while maintaining the structure for potential future use.

--- back-end/app/Services/ServiceBase.php
+++ back-end/app/Services/ServiceBase.php
@@ -69,7 +69,8 @@ class DynamicMethods
  */
 class ServiceBase extends DynamicMethods
 {
-  protected $joinable = [];
+  ## Isso está quebrando os joins que vem do front-end
+  #protected $joinable = [];
   const OPERATORS = ["=", "==", "like", "in", "not in", "<", ">", "<>", "!=", ">=", "<="];
   const ISO8601_VALIDATE = '/^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))((T|\s)(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9])(:(0[0-9]|[1-5][0-9])(\.[0-9]{3})?)?)?Z?$/';
   const ISO8601_FORMAT = "Y-m-d\TH:i:s";
