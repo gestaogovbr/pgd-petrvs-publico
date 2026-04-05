29baddb5ec106695f193a7bf1ef6bd2ed5d02baf
Ricardo Grana <ricardo.lima@firstbps.com.br>
2026-04-04 23:30:03 -0400

    UsuarioEnvioService passa a utilizar afterCommit

--- back-end/app/Services/API_PGD/UsuarioEnvioService.php
+++ back-end/app/Services/API_PGD/UsuarioEnvioService.php
@@ -11,6 +11,6 @@ class UsuarioEnvioService
     public static function processar($tenantId, Usuario $usuario, string $origem = '')
     {
         $job = UsuarioEnvioJobBuilder::make($tenantId, $usuario, $origem);
-        dispatch($job);
+        dispatch($job)->afterCommit();
     }
 }
