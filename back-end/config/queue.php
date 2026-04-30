4986f9ffcbea10340162e7e022ffed8496917c5d
Ricardo Grana <ricardo.lima@firstbps.com.br>
2026-04-29 23:52:37 -0400

    Removidos usuario/senha do definitions do RabbitMQ. Somente foi possível defini-los através de um entrypoint. Container do RabbitMQ tb foi ajustado para ler o .env do caminho correto

--- back-end/config/queue.php
+++ back-end/config/queue.php
@@ -81,7 +81,7 @@ return [
                 [
                     'host' => env('RABBITMQ_HOST', '127.0.0.1'),
                     'port' => env('RABBITMQ_PORT', 5672),
-                    'user' => env('RABBITMQ_USER', 'petrvs'),
+                    'user' => env('RABBITMQ_USER', env('RABBITMQ_LOGIN', 'petrvs')),
                     'password' => env('RABBITMQ_PASSWORD', 'petrvs'),
                     'vhost' => env('RABBITMQ_VHOST', '/'),
                 ],
