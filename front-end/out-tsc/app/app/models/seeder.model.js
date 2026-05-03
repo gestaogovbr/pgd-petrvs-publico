1e4a17933aa7680cff2a05146c44b4f895776005
Ricardo Grana <ricardo.lima@firstbps.com.br>
2026-05-03 11:45:18 -0400

    Remoção dos out-tsc

--- front-end/out-tsc/app/app/models/seeder.model.js
+++
@@ -1,9 +0,0 @@
-import { Base } from './base.model';
-export class Seeder extends Base {
-    constructor(data) {
-        super();
-        this.nome = ""; /* Nome do seeder */
-        this.initialization(data);
-    }
-}
-//# sourceMappingURL=seeder.model.js.map
