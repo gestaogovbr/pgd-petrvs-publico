b8d41f181b9b52adc85ae9fce09195253d5def4a
José Davi <jose.francelino@dataprev.gov.br>
2026-05-22 09:53:48 -0300

    fix(#2167): made pedagio fields fillable

--- back-end/app/Models/Usuario.php
+++ back-end/app/Models/Usuario.php
@@ -149,6 +149,9 @@ class Usuario extends Authenticatable implements AuditableContract, HasStatusHis
         'usuario_externo',
         'is_admin',
         'pedagio',
+        'data_inicial_pedagio',
+        'data_final_pedagio',
+        'tipo_pedagio',
         'data_ativacao_temporaria' /* date; */
     ];
 
