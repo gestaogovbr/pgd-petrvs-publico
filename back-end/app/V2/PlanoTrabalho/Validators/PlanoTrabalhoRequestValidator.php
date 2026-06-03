6619e8dd2047371f5774e21020b460cf01dd5716
Leon Siqueira <leon.siqueira@dataprev.gov.br>
2026-06-03 09:11:33 -0300

    feat(#1683): adiciona opções Avaliado e Encerrado ao filtro de status da listagem

--- back-end/app/V2/PlanoTrabalho/Validators/PlanoTrabalhoRequestValidator.php
+++ back-end/app/V2/PlanoTrabalho/Validators/PlanoTrabalhoRequestValidator.php
@@ -26,7 +26,7 @@ class PlanoTrabalhoRequestValidator
             'filters.incluir_subordinadas' => ['sometimes', 'boolean'],
             'filters.numero' => ['sometimes', 'nullable', 'integer'],
             'filters.modalidade_pgd' => ['sometimes', 'nullable', 'string', Rule::in(ModalidadePgd::keys())],
-            'filters.status' => ['sometimes', 'nullable', 'string', Rule::in(array_keys(PlanoTrabalho::STATUSES))],
+            'filters.status' => ['sometimes', 'nullable', 'string', Rule::in(array_merge(['ENCERRADO'], array_keys(PlanoTrabalho::STATUSES)))],
             'filters.hierarquia' => ['sometimes', 'boolean'],
             'filters.usuario_nome' => ['sometimes', 'nullable', 'string'],
             'filters.unidade_regramento' => ['sometimes', 'nullable', 'string'],
