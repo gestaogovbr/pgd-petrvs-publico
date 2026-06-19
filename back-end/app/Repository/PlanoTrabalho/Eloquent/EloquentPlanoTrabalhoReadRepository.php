015ddda9fbd5b6336ffafb025f2548ade273c464
Ricardo Grana <ricardo.lima@firstbps.com.br>
2026-06-19 11:48:43 -0400

    Otimização na carga de consulta para envio

--- back-end/app/Repository/PlanoTrabalho/Eloquent/EloquentPlanoTrabalhoReadRepository.php
+++ back-end/app/Repository/PlanoTrabalho/Eloquent/EloquentPlanoTrabalhoReadRepository.php
@@ -38,18 +38,21 @@ class EloquentPlanoTrabalhoReadRepository extends AbstractEloquentReadRepository
         /** @var PlanoTrabalho|null */
         $planoTrabalho = $this->model->newQuery()
             ->with([
-                'usuario',
+                'unidade',
+                'usuario.lotacao.unidade',
                 'entregas' => function ($query) {
-                    $query
-                        ->whereNull('plano_entrega_entrega_id')
-                        ->orWhereHas('planoEntregaEntrega.planoEntrega', function ($query) {
-                            $query->whereIn('status', StatusEnum::permitemEnvio());
-                        });
+                    $query->where(function ($query) {
+                        $query
+                            ->whereNull('plano_entrega_entrega_id')
+                            ->orWhereHas('planoEntregaEntrega.planoEntrega', function ($query) {
+                                $query->whereIn('status', StatusEnum::permitemEnvio());
+                            });
+                    });
                 },
+                'entregas.planoEntregaEntrega.planoEntrega',
                 'consolidacoes' => function ($query) {
                     $query->whereIn('status', [StatusEnum::AVALIADO->value]);
                 },
-                'consolidacoes.avaliacoes',
                 'consolidacoes.avaliacao',
             ])
             ->find($id);
