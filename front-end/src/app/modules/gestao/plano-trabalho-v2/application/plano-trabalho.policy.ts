8fd50f8b7bfa7124b586a97fd6c98fb870387ebc
José Davi <jose.francelino@dataprev.gov.br>
2026-06-23 14:54:07 -0300

    fix(#2291): tirar condição para cancelamento de planos suspensos - mesmo com consolidações finalizadas, planos suspensos podem ser cancelados

--- front-end/src/app/modules/gestao/plano-trabalho-v2/application/plano-trabalho.policy.ts
+++ front-end/src/app/modules/gestao/plano-trabalho-v2/application/plano-trabalho.policy.ts
@@ -20,7 +20,7 @@ export class PlanoTrabalhoPolicy {
   podeCancelar(p: PlanoTrabalho): boolean {
     return this.auth.hasPermissionTo('MOD_PTR_CNC')
       && PlanoTrabalhoStatusGroups.cancelavel.includes(p.status)
-      && !p.has_consolidacao_concluida
+      && (p.status != PlanoTrabalhoStatus.ATIVO || !p.has_consolidacao_concluida)
       && (this.auth.usuario?.id == p.usuario_id || this.unidadeService.isGestorUnidade(p.unidade_id));
   }
 
