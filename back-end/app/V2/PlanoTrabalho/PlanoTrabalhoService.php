032e0e95197e2f3ec2a00936d14def9bd8e61484
Leon Siqueira <leon.siqueira@dataprev.gov.br>
2026-07-29 14:55:35 -0300

    refactor(#2428): substituir acesso dinâmico por interface HasOwnership no ValidaAutorizacaoTrait

--- back-end/app/V2/PlanoTrabalho/PlanoTrabalhoService.php
+++ back-end/app/V2/PlanoTrabalho/PlanoTrabalhoService.php
@@ -146,6 +146,7 @@ class PlanoTrabalhoService
 
     public function show(string $id): PlanoTrabalho
     {
+        /** @var \App\Models\PlanoTrabalho|null $plano */
         $plano = $this->readRepository->findByIdComRelacoes($id);
 
         if ($plano === null) {
--- back-end/app/V2/PlanoTrabalho/Validators/PlanoTrabalhoCancelarValidator.php
+++ back-end/app/V2/PlanoTrabalho/Validators/PlanoTrabalhoCancelarValidator.php
@@ -30,6 +30,7 @@ class PlanoTrabalhoCancelarValidator
 
     public function validar(string $planoId, string $usuarioLogadoId): PlanoTrabalho
     {
+        /** @var \App\Models\PlanoTrabalho|null $plano */
         $plano = $this->planoTrabalhoRepository->findById($planoId);
 
         if ($plano === null) {
