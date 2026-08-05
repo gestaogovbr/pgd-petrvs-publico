597f98cc0304a0a7c20ef6a5c6df0d4a3be5e578
Igor Gomes <igor.brito@qintess.com>
2026-08-04 23:15:35 -0300

    fix(#2419): permite edição de usuários com Perfil Colaborador interno

--- back-end/app/Services/UsuarioService.php
+++ back-end/app/Services/UsuarioService.php
@@ -825,8 +825,6 @@ class UsuarioService extends ServiceBase
 
         if ($perfil->nivel < 6 && $usuarioExterno == 1) {
             throw new ServerException("ValidateUsuario", "Usuário externo não pode ter o nível de acesso: " . $perfil->nome);
-        } elseif ($perfil->nivel == 6 && $usuarioExterno == 0) {
-            throw new ServerException("ValidateUsuario", "Usuário não pode ter o nível de acesso: " . $perfil->nome);
         }
     }
 
