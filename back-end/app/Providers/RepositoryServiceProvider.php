a41028c41463bd3aef3030ba8f5cd03472acc5b8
Ricardo Grana <ricardo.lima@firstbps.com.br>
2026-08-17 18:53:46 -0400

    COrreção de bug na meta de Registros de Execução

--- back-end/app/Providers/RepositoryServiceProvider.php
+++ back-end/app/Providers/RepositoryServiceProvider.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Repository\PlanoEntregaEntrega\Contracts\PlanoEntregaEntregaReadRepositoryContract;
+use App\Repository\PlanoEntregaEntrega\Contracts\PlanoEntregaEntregaWriteRepositoryContract;
+use App\Repository\PlanoEntregaEntrega\Eloquent\EloquentPlanoEntregaEntregaReadRepository;
+use App\Repository\PlanoEntregaEntrega\Eloquent\EloquentPlanoEntregaEntregaWriteRepository;
+use App\Repository\PlanoEntregaEntregaProgresso\Contracts\PlanoEntregaEntregaProgressoReadRepositoryContract;
+use App\Repository\PlanoEntregaEntregaProgresso\Eloquent\EloquentPlanoEntregaEntregaProgressoReadRepository;
 use App\Repository\Afastamento\Contracts\AfastamentoReadRepositoryContract;
 
 use App\Repository\Afastamento\Contracts\AfastamentoWriteRepositoryContract;
@@ -473,6 +479,20 @@ final class RepositoryServiceProvider extends ServiceProvider
             EloquentFeriadoReadRepository::class,
         );
 
+
+        $this->app->bind(
+            PlanoEntregaEntregaReadRepositoryContract::class,
+            EloquentPlanoEntregaEntregaReadRepository::class,
+        );
+        $this->app->bind(
+            PlanoEntregaEntregaWriteRepositoryContract::class,
+            EloquentPlanoEntregaEntregaWriteRepository::class,
+        );
+
+        $this->app->bind(
+            PlanoEntregaEntregaProgressoReadRepositoryContract::class,
+            EloquentPlanoEntregaEntregaProgressoReadRepository::class,
+        );
     }
 
     public function boot(): void
