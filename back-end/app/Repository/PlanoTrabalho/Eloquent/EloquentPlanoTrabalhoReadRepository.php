a583c25edad304bda97bb669e2ec76264385bb02
José Davi <jose.francelino@dataprev.gov.br>
2026-08-26 14:39:02 -0300

    fix(#2497): corrigir filtro 'meus subordinados' na listagem de PTs
    A subquery do filtro minha_equipe em buscarPlanosListagem considerava
    integrantes cujas atribuições foram soft-deleted (órfãos do SIAPE) e
    retornava PTs de qualquer unidade do agente, não apenas da unidade
    gerenciada.
    
    - Adicionado JOIN com unidades_integrantes_atribuicoes filtrando
      deleted_at IS NULL para garantir que o integrante possui atribuição
      ativa
    - Adicionado whereIn na unidade_id do PT para restringir resultados
      às unidades efetivamente gerenciadas
    - Adicionados testes para os cenários de integrantes órfãos e PTs
      de outras unidades

--- back-end/app/Repository/PlanoTrabalho/Eloquent/EloquentPlanoTrabalhoReadRepository.php
+++ back-end/app/Repository/PlanoTrabalho/Eloquent/EloquentPlanoTrabalhoReadRepository.php
@@ -278,11 +278,15 @@ class EloquentPlanoTrabalhoReadRepository extends AbstractEloquentReadRepository
         if ($filtro->unidadesId !== null) {
             if ($filtro->minhaEquipe) {
                 $query->whereIn('usuario_id', function ($sub) use ($filtro) {
-                    $sub->select('usuario_id')
-                        ->from('unidades_integrantes')
-                        ->whereIn('unidade_id', $filtro->unidadesId)
-                        ->whereNull('deleted_at');
-                });
+                    $sub->select('ui.usuario_id')
+                        ->from('unidades_integrantes as ui')
+                        ->join('unidades_integrantes_atribuicoes as uia', function ($join) {
+                            $join->on('uia.unidade_integrante_id', '=', 'ui.id')
+                                ->whereNull('uia.deleted_at');
+                        })
+                        ->whereIn('ui.unidade_id', $filtro->unidadesId)
+                        ->whereNull('ui.deleted_at');
+                })->whereIn('planos_trabalhos.unidade_id', $filtro->unidadesId);
             } else {
                 $query->whereIn('unidade_id', $filtro->unidadesId);
             }
