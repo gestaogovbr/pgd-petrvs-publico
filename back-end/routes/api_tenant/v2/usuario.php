809770ee769013f1241e30738396409b8cf5ce49
Ricardo Grana <ricardo.lima@firstbps.com.br>
2026-08-27 10:37:23 -0400

    Subindo rotas novas

--- back-end/routes/api_tenant/v2/usuario.php
+++ back-end/routes/api_tenant/v2/usuario.php
@@ -3,7 +3,12 @@
 use App\V2\Usuario\UsuarioController as UsuarioV2;
 use Illuminate\Support\Facades\Route;
 
-Route::get('usuario', [UsuarioV2::class, 'buscarPorNomeMatricula']);
-Route::get('usuario/cpf/{cpf}/unidades', [UsuarioV2::class, 'buscarUnidadesVinculadasPorCpf']);
-Route::get('usuario/{usuarioId}', [UsuarioV2::class, 'buscarPorId'])->whereUuid('usuarioId');
-Route::patch('usuario/nome-social', [UsuarioV2::class, 'atualizarNomeSocial']);
+Route::get('usuario', [UsuarioV2::class, 'searchByNomeMatricula']);
+Route::get('usuario/cpf/{cpf}/unidades', [UsuarioV2::class, 'unidadesVinculadasPorCpf']);
+Route::get('usuario/{usuarioId}', [UsuarioV2::class, 'show'])->whereUuid('usuarioId');
+Route::post('usuario', [UsuarioV2::class, 'store']);
+Route::patch('usuario/nome-social', [UsuarioV2::class, 'updateNomeSocial']);
+Route::patch('usuario/{usuarioId}/dados-pessoais', [UsuarioV2::class, 'updateDadosPessoais'])->whereUuid('usuarioId');
+Route::patch('usuario/{usuarioId}/texto-complementar', [UsuarioV2::class, 'updateTextoComplementar'])->whereUuid('usuarioId');
+Route::patch('usuario/{usuarioId}/perfil', [UsuarioV2::class, 'updatePerfil'])->whereUuid('usuarioId');
+Route::put('usuario/{usuarioId}/atribuicoes', [UsuarioV2::class, 'updateAtribuicoes'])->whereUuid('usuarioId');
