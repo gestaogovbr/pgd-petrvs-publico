6e56fddbe73ba0eb5549bfcaa95450aee02e8a26
Ricardo Grana <ricardo.lima@firstbps.com.br>
2026-08-28 02:14:55 -0400

    Correções de testes

--- back-end/app/V2/Unidade/UnidadeController.php
+++ back-end/app/V2/Unidade/UnidadeController.php
@@ -6,6 +6,7 @@ namespace App\V2\Unidade;
 
 use App\Exceptions\Contracts\IBaseException;
 use App\Http\Controllers\Controller;
+use App\V2\Unidade\DTOs\UnidadeBuscaDTO;
 use App\V2\Unidade\DTOs\UnidadeIndexDTO;
 use App\V2\Unidade\Validators\UnidadeRequestValidator;
 use Illuminate\Http\JsonResponse;
@@ -26,6 +27,25 @@ class UnidadeController extends Controller
             $data = UnidadeRequestValidator::index($request);
             $dto = UnidadeIndexDTO::fromRequest($data);
             $result = $this->service->index($dto);
+
+            return response()->json(['success' => true, 'data' => $result]);
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->getMessage()], $e->status);
+        } catch (IBaseException $e) {
+            return response()->json(['error' => $e->getMessage()], $e->getCode());
+        } catch (Throwable $e) {
+            report($e);
+
+            return response()->json(['error' => 'Ocorreu um erro inesperado.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function buscarPorNomeOuCodigo(Request $request): JsonResponse
+    {
+        try {
+            $data = UnidadeRequestValidator::buscarPorNomeOuCodigo($request);
+            $dto = UnidadeBuscaDTO::fromArray($data);
+            $result = $this->service->buscarPorNomeOuCodigo($dto);
 
             return response()->json(['success' => true, 'data' => $result]);
         } catch (ValidationException $e) {
--- back-end/app/V2/Unidade/UnidadeService.php
+++ back-end/app/V2/Unidade/UnidadeService.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\V2\Unidade;
 
 use App\Repository\UnidadeRepository;
+use App\V2\Unidade\DTOs\UnidadeBuscaDTO;
 use App\V2\Unidade\DTOs\UnidadeIndexDTO;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,6 +20,11 @@ class UnidadeService
     public function index(UnidadeIndexDTO $dto): LengthAwarePaginator
     {
         return $this->unidadeRepository->index($dto);
+    }
+
+    public function buscarPorNomeOuCodigo(UnidadeBuscaDTO $dto): Collection
+    {
+        return $this->unidadeRepository->buscarPorNomeOuCodigo($dto);
     }
 
     public function isGestorHierarquia(string $unidadeId): bool
