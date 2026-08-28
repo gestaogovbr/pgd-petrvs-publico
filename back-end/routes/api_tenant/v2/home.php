ec167106fe3dcc4add9a9a57f9478689aae23c2a
Leon Siqueira <leon.siqueira@dataprev.gov.br>
2026-08-28 10:50:19 -0300

    merge 2360 adiciona rota ao hmg pós refactor

--- back-end/routes/api_tenant/v2/home.php
+++ back-end/routes/api_tenant/v2/home.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('home/pendencias', [HomeV2::class, 'pendencias']);
 Route::get('home/pendencias-global', [HomeV2::class, 'pendenciasGlobal']);
 Route::get('home/planos-vigentes', [HomeV2::class, 'planosVigentes']);
+Route::get('home/meus-planos-vigentes', [HomeV2::class, 'meusPlanosVigentes']);
 Route::get('home/resumo-equipe', [HomeV2::class, 'resumoEquipe']);
 Route::get('home/contribuicoes', [HomeV2::class, 'contribuicoes']);
 Route::get('home/aniversariantes', [HomeV2::class, 'aniversariantes']);
